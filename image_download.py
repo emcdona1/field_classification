2bb20f824a10a2af35f8c5130b0867408022faf9
allisonchen23 <achen227@yahoo.com>
2019-07-17 15:21:36 -0500

    included arg parse stuff

--- image_download.py
+++ image_download.py
@@ -1,25 +1,26 @@
 import csv
 import pandas as pd
 import requests #must do 'pip install requests' in terminal
+import argparse
 
-with open('lycopodiaceae_images_univ_alaska.csv') as image_csv:
-    rows = pd.read_csv(image_csv)#, delimiter = ',', quotechar='"')
-    print(rows.head())
-    # n_rows=list(rows)
-    # image_url=n_rows[1][1]
-    # coreid=n_rows[1][0]
-    # print(image_url)
+def download_images_from_csv(csv_local_name, download_location):
+    with open(csv_local_name) as image_csv:
+        rows = pd.read_csv(image_csv, usecols=['coreid','identifier'])
+        # print(rows.head())
+        for i in range(len(rows)):
+            image_url = rows.identifier[i]
+            coreid = rows.coreid[i]
+            result = requests.get(image_url)
+            if (result.status_code == 200):
+                with open(download_location+str(coreid)+'.png', 'wb') as download:
+                    download.write(result.content)
 
-    # result = requests.get(image_url)
-    # if (result.status_code == 200):
-    #     #image = result.raw.read()
-    #     with open('image_downloads/'+coreid+'.png', 'wb') as download:
-    #         download.write(result.content)
-    for i in range(len(rows)):
-        image_url = rows.identifier[i]
-        coreid = rows.coreid[i]
-        result = requests.get(image_url)
-        if (result.status_code == 200):
-            #image = result.raw.read()
-            with open('image_downloads/'+str(coreid)+'.png', 'wb') as download:
-                download.write(result.content)
+if __name__== '__main__':
+    parser = argparse.ArgumentParser('data to be imported')
+    parser.add_argument('-f', '--file', default= 'lycopodiaceae_images_univ_alaska_short.csv', help='Upload CSV with URLs in 2nd column')
+    parser.add_argument('-l', '--location', default= '', help = 'type the location you want images downloaded in')
+    args = parser.parse_args()
+    download_images_from_csv(args.file, args.location)
+
+#calling syntax
+# python image_download.py -f [file_name] -l [location]
