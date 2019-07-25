e072e24d0d1267112a7e29243c4764f5a6607a5c
Allison Chen <43938767+allisonchen23@users.noreply.github.com>
2019-07-25 13:50:27 -0500

    added appropriate encoding (utf-8) to read csvs

--- image_download.py
+++ image_download.py
@@ -4,7 +4,7 @@ import requests #must do 'pip install requests' in terminal
 import argparse
 
 def download_images_from_csv(csv_local_name, occ_file_name, download_location):
-    with open(csv_local_name) as image_csv: #using with ensures files get closed automatically at end - good for memory
+    with open(csv_local_name, encoding='utf-8') as image_csv: #using with ensures files get closed automatically at end - good for memory
         image_rows = pd.read_csv(image_csv, usecols=['coreid','identifier','goodQualityAccessURI', 'format']) #we only want these three columns
         #delete the duplicate rows
         image_rows = image_rows[image_rows.format=='image/jpeg']
@@ -12,7 +12,7 @@ def download_images_from_csv(csv_local_name, occ_file_name, download_location):
         image_rows = image_rows.reset_index(drop=True)
         image_rows = image_rows.drop('format', axis=1)
         print("Filtered duplicates")
-        occ_df = pd.read_csv(open(occ_file_name), usecols=['id','catalogNumber'])
+        occ_df = pd.read_csv(open(occ_file_name, encoding = 'utf-8'), usecols=['id','catalogNumber'])
         barcode_dict=occ_df.set_index('id').T.to_dict('list')
         #prep a way to keep track of images we couldn't find
         num_not_found=0
@@ -45,7 +45,7 @@ def download_images_from_csv(csv_local_name, occ_file_name, download_location):
 
 if __name__== '__main__':
     parser = argparse.ArgumentParser('data to be imported')
-    parser.add_argument('-f', '--file', default= 'lycopodiaceae_images_univ_alaska_short.csv', help='Upload CSV with URLs in 2nd column')
+    parser.add_argument('-f', '--file', default= 'dli_images/images_final.csv', help='Upload CSV with URLs in 2nd column')
     parser.add_argument('-o', '--occurrences', help="name of occurrences file to help put the barcode on")
     parser.add_argument('-l', '--location', default= '', help = 'type the location you want images downloaded in (NO SLASH AT END)')
     args = parser.parse_args()
