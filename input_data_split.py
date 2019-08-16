a5437c302b2994e1e67e5b503fe8defb1efd50bd
Allison Chen <43938767+allisonchen23@users.noreply.github.com>
2019-08-16 14:21:36 -0500

    removed extraneous comments

--- input_data_split.py
+++ input_data_split.py
@@ -7,31 +7,6 @@ import pickle
 import math
 import argparse
 
-# DATA_DIR = 'data'
-# # CATEGORIES = ['Lycopodiaceae', 'Selaginellaceae']
-# CATEGORIES = ['lyco_train', 'sela_train']
-# IMG_SIZE = 256 #pixels
-
-#create an array that holds two items: arrays for each pixel in each image and the label for that image
-# def split_data(training_data, testing_data, percent_test): #percent_test is how much of the data goes to testing data
-#     all_data = []
-#     if (percent_test>1):
-#         print("Invalid percent")
-#         return
-#     for category in CATEGORIES:
-#         path=os.path.join(DATA_DIR,category) #look at each folder of images
-#         class_index = CATEGORIES.index(category)
-#         for img in os.listdir(path): # look at each image
-#             try:
-#                 img_array = cv2.imread(os.path.join(path,img), -1) #-1 means image is read as color
-#                 all_data.append([img_array, class_index,img])
-#             except Exception as e:
-#                 pass
-#     random.shuffle(all_data)
-#     num_training=math.floor(len(all_data)*(1.0-percent_test))
-#     training_data = all_data
-#     print("Loaded and shuffled data")
-
 def split_data(all_data, data_dir, categories): 
     for category in categories:
         path=os.path.join(data_dir,category) #look at each folder of images
@@ -95,23 +70,15 @@ def store_data(data_dir, all_data, percent_test,image_size): #percent_test is ho
     pickle_out.close()
     print("Data stored in pickle files")
 
-
-# training_data= []
-# training_data = create_training_data(training_data)
-# store_training_data(training_data)
-
-# # just checking that we all good
-# pickle_in = open("features.pickle", "rb")
-# X = pickle.load(pickle_in)
-# print('hi')
 if __name__== '__main__':
     parser = argparse.ArgumentParser('data to be imported')
     parser.add_argument('-d', '--directory', default='frullania', help='Folder holding category folders')
     parser.add_argument('-c1', '--category1', default='coastal_rsz', help='1st folder')
     parser.add_argument('-c2', '--category2', default='rostrata_rsz', help='2nd folder')
     parser.add_argument('-s', '--image_size', default = '256', help="Image size")
+    parser.add_argument('-p', '--percent_test', default =0.1, help='percentage between [0,1] to go to testing')
     args = parser.parse_args()
     categories = [args.category1, args.category2]
     all_data= []
     all_data = split_data(all_data, args.directory, categories)
-    store_data(args.directory, all_data, 0.1, int(args.image_size))
+    store_data(args.directory, all_data, args.percent_test, int(args.image_size))
