2f82930876d42ac50b81c5abf3146a64d764b1cf
Allison Chen <43938767+allisonchen23@users.noreply.github.com>
2019-08-16 13:51:09 -0500

    custom new image size

--- image_resize.py
+++ image_resize.py
@@ -5,13 +5,13 @@ import cv2
 import os, sys
 import pandas as pd
 
-def make_square(file_name, dest_path, dir_path):
+def make_square(file_name, dest_path, dir_path, img_size):
     img = cv2.imread(dir_path + '/' + file_name, -1)
     output_name="N/A"
     try:
         file_name=file_name.replace('.jpg','')
         output_name = file_name+'_rsz.jpg'
-        img_rsz = cv2.resize(img, (256,256), interpolation = cv2.INTER_AREA)
+        img_rsz = cv2.resize(img, (img_size,img_size), interpolation = cv2.INTER_AREA)
         cv2.imwrite(dest_path+'/' + output_name, img_rsz)
     except:
         print(file_name + " was unable to be resized")
@@ -33,5 +33,6 @@ if __name__== '__main__':
     parser.add_argument('-f', '--folder', help="Path to folder where original images are stored")
     parser.add_argument('-d', '--destination', default='', help='Folder/path where you want image saved')
     parser.add_argument('-t', '--tag', default = '', help="The label for all these images to be in the CSV File")
+    parser.add_argument('-s', '--image_size', default=256, help='The new image height/width (assuming output is square)')
     args = parser.parse_args()
-    resize_folder(args.folder, args.destination, args.tag)
+    resize_folder(args.folder, args.destination, args.tag, args.image_size)
