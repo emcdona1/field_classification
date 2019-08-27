72f2f6a1285b0840e1309d4ca56f1dec407cabe4
Allison Chen <43938767+allisonchen23@users.noreply.github.com>
2019-08-27 10:53:07 -0500

    remove saving names to csv

--- image_resize.py
+++ image_resize.py
@@ -3,7 +3,6 @@ from resizeimage import resizeimage
 import argparse
 import cv2
 import os, sys
-import pandas as pd
 
 def make_square(file_name, dest_path, dir_path, img_size):
     img = cv2.imread(dir_path + '/' + file_name, -1)
@@ -19,23 +18,15 @@ def make_square(file_name, dest_path, dir_path, img_size):
 
 def resize_folder(dir_path, dest_path, label, img_size):
     all_images = os.listdir(dir_path)
-    df=pd.DataFrame()
     for i in range(len(all_images)):
         output_name = make_square(all_images[i], dest_path, dir_path, img_size)
-        df = df.append([[output_name, label]])
         if i % 500 == 0:
             print (i, " images resized")
-    df = df.rename({0: 'Image Name', 1: 'Family'}, axis='columns')
-    # df.to_csv(dest_path + '/_label.csv', encoding= 'utf-8', index=False)
 
 if __name__== '__main__':
     parser = argparse.ArgumentParser('data to be imported')
     parser.add_argument('-f', '--folder', help="Path to folder where original images are stored")
     parser.add_argument('-d', '--destination', default='', help='Folder/path where you want image saved')
-    parser.add_argument('-t', '--tag', default = '', help="The label for all these images to be in the CSV File")
     parser.add_argument('-s', '--image_size', default=256, help='The new image height/width (assuming output is square)')
     args = parser.parse_args()
-    # if the destination folder doesn't exist yet, create it
-    if not os.path.exists(args.destination):
-        os.makedirs(args.destination)
     resize_folder(args.folder, args.destination, args.tag, int(args.image_size))
