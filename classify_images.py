e74792ba74b3bfdb31765c3e97008b6238eef0e9
emcdona1 <emcdona1@neiu.edu>
2019-09-17 10:52:36 -0500

    Add arguments for flexibility

--- classify_images.py
+++ classify_images.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from datetime import datetime
 import cv2
 import numpy as np
@@ -6,10 +7,8 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 
-def import_images():
+def import_images(img_directory, folders):
     all_data = []
-    folders = ['rostrata_micro', 'coastal_micro']
-    img_directory = '.\\images\\'
     for category in folders:
         path=os.path.join(img_directory,category) #look at each folder of images
         class_index = folders.index(category)
@@ -38,24 +37,33 @@ def import_images():
     return features, labels, img_names
 
 if __name__ == '__main__':
-    '''Currently this only works for hardcoding in the model used, and the frullania project.'''
+    parser = argparse.ArgumentParser('Import a model and classify images.')
+    parser.add_argument('-d', '--directory', default='images', help='Folder holding category folders')	
+    parser.add_argument('-c1', '--category1', help='Folder of class 1')
+    parser.add_argument('-c2', '--category2', help='Folder of class 2')
+    parser.add_argument('-m', '--model', help='Filepath of model to use')
+    args = parser.parse_args()
+    img_directory = args.directory
+    folders = [args.category1, args.category2]
+    model_directory = args.model
+
     # Load model
-    model = tf.keras.models.load_model('.\\saved_models\\CNN_1.model')
+    model = tf.keras.models.load_model(model_directory)
 
     # Import images
-    pics, actual_class, img_names = import_images()
+    pics, actual_class, img_names = import_images(img_directory, folders)
     
     # Predict classes of imported images
     predictions = model.predict(pics)
 
     # Map class numbers to class labels
-    maps = ['rostrata', 'coastal']
+    maps = [ folders[0].split('_')[0], folders[1].split('_')[0] ]
     mfunc = np.vectorize(lambda t: maps[int(round(t))])
     prediction_class_labels = mfunc(predictions[:,[1]])
     actual_class_labels = mfunc(actual_class)
 
     # Join all information into one nparray -> pd.DataFrame
-    headers = ['filename', 'rostrata_pred', 'coastal_pred', 'class_pred', 'actual', 'actual_class']
+    headers = ['filename', maps[0] + '_pred', maps[1] + '_pred', 'class_pred', 'actual', 'actual_class']
     pred_joined = np.c_[img_names, predictions, prediction_class_labels, actual_class, actual_class_labels]
     predictions_final = pd.DataFrame(pred_joined, columns=headers)
 
