bcf25ea1c67c92d005c44c267e419493ada46ac2
emcdona1 <emcdona1@neiu.edu>
2019-10-08 13:36:18 -0500

    first version: output probs from all 5 models

--- classify_images_by_vote.py
+++ classify_images_by_vote.py
@@ -1,12 +1,23 @@
-THRESHOLD = 0.25
+import os
+import argparse
+import time
+from datetime import datetime
+import classify_images
+import cv2
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+
+THRESHOLD = (1 - 0.7395)
 
 def process_input_arguments():
     parser = argparse.ArgumentParser('Import a model and classify images.')
-    parser.add_argument('-d', '--directory', default='images', help='Folder holding image folders')	
-    parser.add_argument('-c1', '--category1', help='Folder of class 1')
-    parser.add_argument('-c2', '--category2', help='Folder of class 2')
+    parser.add_argument('-d', '--directory', default='images', help='Folder containing the image folders')	
+    parser.add_argument('-c1', '--category1', help='Folder of class 1 images')
+    parser.add_argument('-c2', '--category2', help='Folder of class 2 images')
     parser.add_argument('-s', '--img_size', default=256, help='Image dimension in pixels')
-    parser.add_argument('-m', '--models', help='Directory of models to use')
+    parser.add_argument('-m', '--models', help='Folder of models to use')
     args = parser.parse_args()
 
     img_directory = args.directory
@@ -20,20 +31,38 @@ if __name__ == '__main__':
     # Start execution and parse arguments
     start_time = time.time()
     img_directory, folders, img_size, model_directory = process_input_arguments()
+    # Map class numbers to class labels
+    class_labels = [ folders[0].split('_')[0], folders[1].split('_')[0] ]
 
     # Import images
-    pixel_values, actual_class, img_filenames = import_images(img_directory, folders, img_size)
+    pixel_values, actual_class, img_filenames = classify_images.import_images(img_directory, folders, img_size)
     print('Images imported.')
 
-    # for each model in model_directory:
-    #   load model
-    #   classify each image (make a 5-col chart)
-    # for each row in chart, vote (simple majority) and give each *image* a final classification
-    # first output: image name, final classification, true classification, tp/fn/fp/tn, then each classification
-    
-    # then, aggregate by *plant* - the first part of the filename up to the underscore
-    # simple majority to classify each plant
-    # second output: plant specimen name, final classification, true classification, tp/fn/fp/tn
+    combined_results = pd.DataFrame()
+
+    for model_name in os.listdir(model_directory):
+        model_path = os.path.join(model_directory, model_name)
+        if os.path.isfile(model_path):
+            # Load model
+            model = tf.keras.models.load_model(model_path)
+            print('Model ' + model_name + ' loaded.')
+
+            # Generate predictions and organize results
+            predictions = classify_images.make_predictions(pixel_values, actual_class, img_filenames, class_labels, model)
+            print('Predictions generated.')
+
+            # if this is the first model being processed, add a column of the file names & actual classification
+            if len(combined_results) == 0:
+                combined_results['filename'] = predictions['filename']
+                combined_results['actual_class'] = predictions['actual_class']
+            # add newest predictions to results
+            combined_results[model_name] = predictions[class_labels[1] + '_pred'] # probability of class = 1
+
+    # TODO: Get rid of this temporary hard-coding!
+    classify_images.write_dataframe_to_CSV('','all_model_predict', combined_results)
+
+    # TODO: for each row in chart, vote (simple majority) and give each *image* a final classification
+    # TODO: output: image name, final classification, true classification, tp/fn/fp/tn, then each classification
 
 
     # Finish execution
