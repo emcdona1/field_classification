2f7a4588015785243df69814fed2d4a2cf38db70
emcdona1 <emcdona1@neiu.edu>
2020-01-10 12:53:07 -0600

    Finish moving graphing/file saving to separate file.

--- create_models_with_cross_validation.py
+++ create_models_with_cross_validation.py
@@ -49,101 +49,32 @@ def create_folders():
     if not os.path.exists('saved_models'):
         os.makedirs('saved_models')
 
-def train_model_on_images(model, train_features, train_labels, num_epochs, val_features, val_labels):
+
+def train_cross_validate(index, training_idx_list, validation_idx_list, charts, n_folds,
+                         features, labels, img_size, color, num_epochs):
+    print("Training on fold " + str(index + 1) + "/" + str(n_folds))
+    train_features = features[training_idx_list]
+    train_labels = labels[training_idx_list]
+    validation_features = features[validation_idx_list]
+    validation_labels = labels[validation_idx_list]
+
+    architecture = SmithsonianModel(img_size, color_mode=color, seed=SEED, lr=LEARNING_RATE)
     print("Training model")
     # es_callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', \
     #        mode='min', min_delta = 0.05, patience = 20, restore_best_weights = True)
-    history = model.fit(train_features, train_labels,
-                        batch_size=BATCH_SIZE, epochs=num_epochs,
-                        #        callbacks = [es_callback], \
-                        validation_data=(val_features, val_labels),
-                        verbose=2)
-    return history
+    history = architecture.model.fit(train_features, train_labels,
+                                     batch_size=BATCH_SIZE, epochs=num_epochs,
+                                     #        callbacks = [es_callback], \
+                                     validation_data=(validation_features, validation_labels),
+                                     verbose=2)
+    architecture.model.save(os.path.join('saved_models', 'CNN_' + str(index + 1) + '.model'))
 
+    # Classify the validation set
+    validation_predicted_probability = architecture.model.predict_proba(validation_features)[:, 1]
+    validation_predicted_classification = [round(a + 0.0001) for a in validation_predicted_probability]
 
-def save_results_to_csv(results):
-    # results = results.rename({0: 'Fold Number',
-    #                           1: 'Training Loss',
-    #                           2: 'Training Accuracy',
-    #                           3: 'Validation Loss',
-    #                           4: 'Validation Accuracy',
-    #                           5: 'True Negatives', 6: 'False Positives',
-    #                           7: 'False Negatives', 8: 'True Positives'})
-    results.to_csv(os.path.join('graphs', 'final_acc_loss.csv'), encoding='utf-8', index=False)
-
-
-def train_cross_validate(n_folds, features, labels, img_size, color, num_epochs):
-    """ Import images from the file system and returns two numpy arrays containing the pixel information and classification.
-
-    Parameters:
-    -----
-    @ n_folds : int
-    Number of folds to train on (minimum of 2)
-
-    @ features : np.array
-    Array containing the pixel values -- dimensions are (img_size x img_size x 3) if color,
-    (img_size x img_size x 1) if grayscale.
-
-    @ labels : np.array
-    Array containing the actual labels (0/1) of the images
-
-    @ img_size : int
-    Pixel dimensions of images
-
-    @ num_epochs : int
-    Maximum number of epochs to perform on each k-fold.
-
-    Output:
-    -----
-    none
-    """
-    charts = ChartCreator()
-
-    # initialize stratifying k fold
-    skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=SEED)
-
-    # data frame to save values of loss and validation after each fold
-    results = pd.DataFrame(columns=['Fold Number', 'Training Loss', 'Training Accuracy',
-                                    'Validation Loss', 'Validation Accuracy',
-                                    'True Negatives', 'False Positives', 'False Negatives', 'True Positives'])
-
-    for index, (train_indices, val_indices) in enumerate(skf.split(features, labels)):
-        print("Training on fold " + str(index + 1) + "/" + str(n_folds))
-        train_features = features[train_indices]
-        train_labels = labels[train_indices]
-        # print("Training data obtained")
-        validation_features = features[val_indices]
-        validation_labels = labels[val_indices]
-        # print("Validation data obtained")
-
-        # Create new model each time
-        architecture = SmithsonianModel(img_size, color_mode=color, seed=SEED, lr=LEARNING_RATE)
-        history = train_model_on_images(architecture.model, train_features, train_labels,
-                                        num_epochs, validation_features, validation_labels)
-
-        architecture.model.save(os.path.join('saved_models', 'CNN_' + str(index + 1) + '.model'))
-
-        charts.plot_accuracy(history, index)
-        charts.plot_loss(history, index)
-
-        # Classify the validation set
-        validation_predicted_probability = architecture.model.predict_proba(validation_features)[:, 1]
-        validation_predicted_classification = [round(a + 0.0001) for a in validation_predicted_probability]
-
-        # Plot ROC curve, generate confusion matrix
-        charts.plot_roc_curve(index, validation_labels, validation_predicted_probability)
-        tn, fp, fn, tp = charts.generate_confusion_matrix(validation_labels, validation_predicted_classification)
-
-        num_epochs = len(history.history['loss'])
-        # save the stats of the last epoch (i.e. end of the fold) to the results file
-        results = results.append([[index + 1,
-                                   history.history['loss'][num_epochs - 1],
-                                   history.history['acc'][num_epochs - 1],
-                                   history.history['val_loss'][num_epochs - 1],
-                                   history.history['val_acc'][num_epochs - 1],
-                                   tn, fp, fn, tp]])
-
-    save_results_to_csv(results)
+    charts.update_and_save_graphs(history, index, validation_labels,
+                                  validation_predicted_classification, validation_predicted_probability)
 
 
 if __name__ == '__main__':
@@ -159,7 +90,12 @@ if __name__ == '__main__':
     labels = images.labels
 
     # Train model
-    train_cross_validate(n_folds, features, labels, img_size, color, n_epochs)
+    skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=SEED)
+    charts = ChartCreator()
+    for index, (training_idx_list, validation_idx_list) in enumerate(skf.split(features, labels)):
+        train_cross_validate(index, training_idx_list, validation_idx_list, charts, n_folds,
+                             features, labels, img_size, color, n_epochs)
+    charts.save_results_to_csv()
 
     # end
     print('c1: ' + folders[0] + ', c2: ' + folders[1])
--- data_plotter.py
+++ data_plotter.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pandas as pd
 from scipy import interp
 from sklearn.metrics import roc_curve, confusion_matrix, auc
 import matplotlib.pyplot as plt
@@ -8,11 +9,27 @@ import matplotlib.pyplot as plt
 class ChartCreator:
     def __init__(self):
         # for roc plotting
-        self.tprs = []
-        self.aucs = []
+        self.tpr = []
+        self.auc = []
         self.mean_fpr = np.linspace(0, 1, 100)
+        self.index = 0
+        self.history = None
+        self.results = pd.DataFrame()
 
-    def plot_accuracy(self, history, index):
+    def update(self, history, index, tp, fn, fp, tn):
+        self.history = history
+        self.index = (index + 1)  # Change index from 0-based to 1-based
+
+        num_epochs = len(history.history['loss'])
+        # save the stats of the last epoch (i.e. end of the fold) to the results file
+        self.results = self.results.append([[index + 1,
+                                             history.history['loss'][num_epochs - 1],
+                                             history.history['acc'][num_epochs - 1],
+                                             history.history['val_loss'][num_epochs - 1],
+                                             history.history['val_acc'][num_epochs - 1],
+                                             tn, fp, fn, tp]])
+
+    def plot_accuracy(self):
         """Create plot of training/validation accuracy, and save it to the file system.
 
                 Parameters:
@@ -30,16 +47,16 @@ class ChartCreator:
                 A PNG file saved in graphs folder
                 """
         plt.figure(1)
-        plt.plot(history.history['acc'])
-        plt.plot(history.history['val_acc'])
-        plt.title('Training & Validation Accuracy for Fold ' + str(index + 1))
+        plt.plot(self.history.history['acc'])
+        plt.plot(self.history.history['val_acc'])
+        plt.title('Training & Validation Accuracy for Fold ' + str(self.index))
         plt.ylabel('Accuracy (%)')
         plt.xlabel('Epoch')
         plt.legend(['Training', 'Validation'], loc='upper left')
-        plt.savefig(os.path.join('graphs', 'val_accuracy_' + str(index + 1) + '.png'))
+        plt.savefig(os.path.join('graphs', 'val_accuracy_' + str(self.index) + '.png'))
         plt.clf()
 
-    def plot_loss(self, history, index):
+    def plot_loss(self):
         """Create plot of training/validation loss, and save it to the file system.
 
         Parameters:
@@ -57,16 +74,33 @@ class ChartCreator:
         A PNG file saved in graphs folder
         """
         plt.figure(2)
-        plt.plot(history.history['loss'])
-        plt.plot(history.history['val_loss'])
-        plt.title('Training and Validation Loss for Fold' + str(index + 1))
+        plt.plot(self.history.history['loss'])
+        plt.plot(self.history.history['val_loss'])
+        plt.title('Training and Validation Loss for Fold' + str(self.index))
         plt.ylabel('Loss')
         plt.xlabel('Epoch')
         plt.legend(['Training', 'Validation'], loc='upper left')
-        plt.savefig(os.path.join('graphs', 'val_loss_' + str(index + 1) + '.png'))
+        plt.savefig(os.path.join('graphs', 'val_loss_' + str(self.index) + '.png'))
         plt.clf()
 
-    def plot_roc_curve(self, index, validation_labels, prediction_probability):
+    def plot_roc(self, mean_auc, mean_auc_std, mean_tpr):
+        """ Plot ROC and save graph to file system. """
+        plt.figure(3)
+        plt.plot([0, 1], [0, 1], linestyle='--', lw=2,
+                 color='r', label='random chance', alpha=0.8)
+        plt.plot(self.mean_fpr, mean_tpr, color='blue',
+                 label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, mean_auc_std),
+                 lw=2, alpha=0.8)
+        plt.xlim([-0.05, 1.05])
+        plt.ylim([-0.05, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic (ROC) curve after ' + str(self.index) + ' folds')
+        plt.legend(loc="lower right")
+        plt.savefig(os.path.join('graphs', 'mean_ROC.png'))
+        plt.clf()
+
+    def generate_roc_and_auc(self, validation_labels, prediction_probability):
         """ Updates ROC plot after each fold, and saves to file system.
 
     	Parameters:
@@ -90,36 +124,37 @@ class ChartCreator:
     	Saves plot as `mean_ROC.png` in graphs folder.
     	"""
         # Compute values for ROC curve and area under the curve (AUC)
-        fpr, tpr, _ = roc_curve(validation_labels, prediction_probability)
-        roc_auc = auc(fpr, tpr)
+        latest_fpr, latest_tpr, _ = roc_curve(validation_labels, prediction_probability)
+        latest_auc = auc(latest_fpr, latest_tpr)
 
         # Update arrays with latest ROC/AUC values
-        self.tprs.append(interp(self.mean_fpr, fpr, tpr))
-        self.tprs[-1][0] = 0.0
-        self.aucs.append(roc_auc)
+        self.tpr.append(interp(self.mean_fpr, latest_fpr, latest_tpr))
+        self.tpr[-1][0] = 0.0
+        self.auc.append(latest_auc)
 
         # find/update figures for ROC/AUC so far
-        mean_tpr = np.mean(self.tprs, axis=0)
+        mean_tpr = np.mean(self.tpr, axis=0)
         mean_tpr[-1] = 1.0
         mean_auc = auc(self.mean_fpr, mean_tpr)
-        mean_auc_std = float(np.std(self.aucs))
+        mean_auc_std = float(np.std(self.auc))
 
-        # plot ROC and save to file system
-        plt.figure(3)
-        plt.plot([0, 1], [0, 1], linestyle='--', lw=2,
-                 color='r', label='random chance', alpha=0.8)
-        plt.plot(self.mean_fpr, mean_tpr, color='blue',
-                 label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, mean_auc_std),
-                 lw=2, alpha=0.8)
-        plt.xlim([-0.05, 1.05])
-        plt.ylim([-0.05, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('Receiver operating characteristic (ROC) curve after ' + str(index + 1) + ' folds')
-        plt.legend(loc="lower right")
-        plt.savefig(os.path.join('graphs', 'mean_ROC.png'))
-        plt.clf()
+        self.plot_roc(mean_auc, mean_auc_std, mean_tpr)
 
     def generate_confusion_matrix(self, validation_labels, predicted_classification):
         tn, fp, fn, tp = confusion_matrix(validation_labels, predicted_classification).ravel()
         return tn, fp, fn, tp
+
+    def save_results_to_csv(self):
+        self.results.rename(columns={0: 'Fold Number', 1: 'Training Loss', 2: 'Training Accuracy',
+                                     3: 'Validation Loss', 4: 'Validation Accuracy',
+                                     5: 'True Negatives', 6: 'False Positives', 7: 'False Negatives',
+                                     8: 'True Positives'})
+        self.results.to_csv(os.path.join('graphs', 'final_acc_loss.csv'), encoding='utf-8', index=False)
+
+    def update_and_save_graphs(self, history, index, validation_labels,
+                               validation_predicted_classification, validation_predicted_probability):
+        tn, fp, fn, tp = self.generate_confusion_matrix(validation_labels, validation_predicted_classification)
+        self.update(history, index, tp, fn, fp, tn)
+        self.plot_accuracy()
+        self.plot_loss()
+        self.generate_roc_and_auc(validation_labels, validation_predicted_probability)
