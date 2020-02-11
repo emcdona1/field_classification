c1dff0023ab8a525dcb5f3ef44aeee33e17a9f46
emcdona1 <emcdona1@neiu.edu>
2020-02-11 16:15:39 -0600

    Change ImageImporter to ImageSet object.

--- create_models_with_cross_validation.py
+++ create_models_with_cross_validation.py
@@ -6,7 +6,7 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import StratifiedKFold
 import matplotlib
-from image_handling import ImageImporter
+from image_handling import import_images, LabeledImages
 from neural_network_models import SmithsonianModel
 from data_and_visualization_io import DataChartIO
 
@@ -27,11 +27,10 @@ def main() -> None:
     parser = initialize_argparse()
     args = parser.parse_args()
     color = False if args.bw else True
-    classes = (args.c1, args.c2)
     create_folders()
 
     # Load in images and shuffle order
-    images = ImageImporter(args.dir, classes, args.img_size, color, SEED)
+    images = import_images(args.dir, (args.c1, args.c2), args.img_size, color, SEED)
     features = images.features
     labels = images.labels
 
@@ -39,12 +38,15 @@ def main() -> None:
     skf = StratifiedKFold(n_splits=args.n_folds, shuffle=True, random_state=SEED)
     charts = DataChartIO()
     for index, (training_idx_list, validation_idx_list) in enumerate(skf.split(features, labels)):
-        model, history, validation_features, validation_labels = model_training(args.n_epochs)
-        model_validation()
+        model, history, validation_features, validation_labels = model_training(index, args.n_epochs, args.n_folds,
+                                                                                images.features, images.labels,
+                                                                                training_idx_list, validation_idx_list,
+                                                                                args.img_size, color)
+        model_validation(model, validation_features, charts, history, index, validation_labels)
     charts.save_results_to_csv()
 
     # end
-    print('c1: ' + classes[0] + ', c2: ' + classes[1])
+    print('c1: ' + args.c1 + ', c2: ' + args.c2)
     timer.stop_timer()
 
 
@@ -74,7 +76,8 @@ def create_folders():
         os.makedirs('saved_models')
 
 
-def model_training(num_epochs):
+def model_training(curr_epoch, n_epochs, n_folds, features, labels, training_idx_list, validation_idx_list, img_size,
+                   color):
     # set up training/validation
     train_features = features[training_idx_list]
     train_labels = labels[training_idx_list]
@@ -82,20 +85,20 @@ def model_training(num_epochs):
     validation_labels = labels[validation_idx_list]
     architecture = SmithsonianModel(img_size, color_mode=color, seed=SEED, lr=LEARNING_RATE)
 
-    print('Training model for fold' + str(index + 1) + '/' + str(n_folds))
+    print('Training model for fold' + str(curr_epoch + 1) + '/' + str(n_folds))
     # es_callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', \
     #        mode='min', min_delta = 0.05, patience = 20, restore_best_weights = True)
     history = architecture.model.fit(train_features, train_labels,
-                                     batch_size=BATCH_SIZE, epochs=num_epochs,
+                                     batch_size=BATCH_SIZE, epochs=n_epochs,
                                      #        callbacks = [es_callback], \
                                      validation_data=(validation_features, validation_labels),
                                      verbose=2)
-    architecture.model.save(os.path.join('saved_models', 'CNN_' + str(index + 1) + '.model'))
+    architecture.model.save(os.path.join('saved_models', 'CNN_' + str(curr_epoch + 1) + '.model'))
 
     return architecture.model, history, validation_features, validation_labels
 
 
-def model_validation():
+def model_validation(model, validation_features, charts, history, index, validation_labels):
     # Classify the validation set
     validation_predicted_probability = model.predict_proba(validation_features)[:, 1]
     validation_predicted_classification = [round(a + 0.0001) for a in validation_predicted_probability]
--- image_handling.py
+++ image_handling.py
@@ -5,44 +5,46 @@ import random
 import numpy as np
 
 
+# todo: use this one, delete image_set asap
+
 class ColorMode(enum.Enum):
     RGB = 1
     BW = 2
 
 
-class ImageImporter:
-    def __init__(self, img_directory, folders, img_size, import_in_color, seed):
-        self.directory = img_directory
-        self.folders = folders
-        self.img_size = img_size
-        self.color_mode = ColorMode.RGB if import_in_color else ColorMode.BW
+def import_images(directory, folders, img_size, import_in_color, seed):
+    color_mode = ColorMode.RGB if import_in_color else ColorMode.BW
+    random.seed(seed)
+    imgset = LabeledImages()
+    for (index, folder_name) in enumerate(folders):
+        imgset.load_from_filesystem(directory, folder_name, index, color_mode)
 
-        random.seed(seed)
+    imgset.randomize_order()
+
+
+class LabeledImages:
+    def __init__(self):
         self.features = []
         self.labels = []
-        self.load_from_filesystem()
-        self.randomize_order()
 
-    def load_from_filesystem(self):
-        for (index, folder_name) in enumerate(self.folders):
-            image_folder_path = os.path.join(self.directory, folder_name)
-            for img in os.listdir(image_folder_path):
-                img_array = cv2.imread(os.path.join(image_folder_path, img),
-                                       cv2.IMREAD_COLOR if self.color_mode == ColorMode.RGB
-                                       else cv2.IMREAD_GRAYSCALE)
-                img_array = img_array / 255
-
-                self.features.append(img_array)
-                self.labels.append(index)
-        print('Loaded images.')
+    def load_from_filesystem(self, directory, folder_name, class_num, color_mode):
+        image_folder_path = os.path.join(directory, folder_name)
+        for img in os.listdir(image_folder_path):
+            img_array = cv2.imread(os.path.join(image_folder_path, img),
+                                   cv2.IMREAD_COLOR if color_mode == ColorMode.RGB
+                                   else cv2.IMREAD_GRAYSCALE)
+            img_array = img_array / 255
+            self.features.append(img_array)
+            self.labels.append(class_num)
+        print('Loaded images from %s.' % folder_name)
 
     def randomize_order(self):
         index = list(range(len(self.features)))
         random.shuffle(index)
 
-        shuffled_features = [self.features[i] for i in index]
+        shuffled_image_features = [self.features[i] for i in index]
         shuffled_labels = [self.labels[i] for i in index]
-        self.features = np.array(shuffled_features)
+        self.features = np.array(shuffled_image_features)
         self.labels = np.array(shuffled_labels)
 
-        print('Shuffled data.')
+        print('Shuffled image order.')
