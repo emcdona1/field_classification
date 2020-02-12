985f26410d1cffc97ca729c09a12ce172e56829c
emcdona1 <emcdona1@neiu.edu>
2020-02-12 16:16:28 -0600

    Create accuracy class

--- data_and_visualization_io.py
+++ data_and_visualization_io.py
@@ -27,11 +27,12 @@ class Charts:
 
 
 class Chart:
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path
+        self.file_extension = '.png'
 
     @abstractmethod
-    def update(self, index, validation_labels, prediction_probability):
+    def update(self, index, validation_labels, prediction_probability, history):
         pass
 
     @abstractmethod
@@ -42,18 +43,20 @@ class Chart:
     def save(self, index):
         pass
 
+    @abstractmethod
+    def finalize(self):
+        pass
 
 class ROCChart(Chart):
     def __init__(self):
-        super().__init__()
-        self.path = os.path.join('graphs', 'mean_ROC')
-        self.file_extension = '.png'
+        path = os.path.join('graphs', 'mean_ROC')
+        super().__init__(path)
 
         self.tpr = {}
         self.fpr = {}
         self.auc = {}
 
-    def update(self, index, validation_labels, prediction_probability):
+    def update(self, index, validation_labels, prediction_probability, history):
         """ Updates ROC plot after each fold, and saves to file system.
 
         Parameters:
@@ -106,15 +109,48 @@ class ROCChart(Chart):
         plt.savefig(self.path + str(index) + self.file_extension)
         plt.clf()
 
+    def finalize(self):
+        # TODO
+        pass
 
+
+class AccuracyChart(Chart):
+    def __init__(self):
+        path = os.path.join('graphs', 'accuracy')
+        super().__init__(path)
+
+        self.training = {}
+        self.validation = {}
+
+    def update(self, index, validation_labels, prediction_probability, history):
+        """Create plot of training/validation accuracy, and save it to the file system."""
+        plt.figure(1)
+        self.training[index] = history.history['acc']
+        self.validation[index] = history.history['val_acc']
+
+        self.create_chart(index)
+        self.save(index)
+
+    def create_chart(self, index):
+        plt.plot(self.training[index], label='Training Accuracy')
+        plt.plot(self.validation[index], label='Validation Accuracy')
+        plt.title('Accuracy - Fold %i' % index)
+        plt.ylabel('Accuracy (%)')
+        plt.xlabel('Epoch')
+        plt.legend(loc='upper left')
+
+    def save(self, index):
+        plt.savefig(os.path.join('graphs', 'val_accuracy_' + str(index) + '.png'))
+        plt.clf()
+
+    def finalize(self):
+        # TODO
+        pass
 
 
 
 class DataChartIO:
     def __init__(self):
-        # self.tpr = []
-        # self.auc = []
-        # self.mean_fpr = np.linspace(0, 1, 100)
         self.index = 0
         self.history = None
         self.results = pd.DataFrame()
@@ -156,63 +192,6 @@ class DataChartIO:
         plt.savefig(os.path.join('graphs', 'val_loss_' + str(self.index) + '.png'))
         plt.clf()
 
-    # def plot_roc(self, mean_auc, mean_auc_std, mean_tpr):
-    #     """ Plot ROC and save graph to file system. """
-    #     plt.figure(3)
-    #     plt.plot([0, 1], [0, 1], linestyle='--', lw=2,
-    #              color='r', label='random chance', alpha=0.8)
-    #     plt.plot(self.mean_fpr, mean_tpr, color='blue',
-    #              label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, mean_auc_std),
-    #              lw=2, alpha=0.8)
-    #     plt.xlim([-0.05, 1.05])
-    #     plt.ylim([-0.05, 1.05])
-    #     plt.xlabel('False Positive Rate')
-    #     plt.ylabel('True Positive Rate')
-    #     plt.title('Receiver operating characteristic (ROC) curve after ' + str(self.index) + ' folds')
-    #     plt.legend(loc="lower right")
-    #     plt.savefig(os.path.join('graphs', 'mean_ROC.png'))
-    #     plt.clf()
-
-    # def generate_roc_and_auc(self, validation_labels, prediction_probability):
-    #     """ Updates ROC plot after each fold, and saves to file system.
-    #
-    #     Parameters:
-    #     ------
-    #     @ mean_fpr : float
-    #     false positive rate (mean from all folds run so far)
-    #
-    #     @ mean_tpr : float
-    #     true postive rate (mean from all folds run so far)
-    #
-    #     @ mean_auc : float
-    #     area under ROC curve (mean from all folds run so far)
-    #
-    #     @ std_auc : float
-    #     standard deviation of AUC (mean from all folds run so far)
-    #
-    #     Output:
-    #     ------
-    #     none
-    #
-    #     Saves plot as `mean_ROC.png` in graphs folder.
-    #     """
-    #     # Compute values for ROC curve and area under the curve (AUC)
-    #     latest_fpr, latest_tpr, _ = roc_curve(validation_labels, prediction_probability)
-    #     latest_auc = auc(latest_fpr, latest_tpr)
-    #
-    #     # Update arrays with latest ROC/AUC values
-    #     self.tpr.append(interp(self.mean_fpr, latest_fpr, latest_tpr))
-    #     self.tpr[-1][0] = 0.0
-    #     self.auc.append(latest_auc)
-    #
-    #     # find/update figures for ROC/AUC so far
-    #     mean_tpr = np.mean(self.tpr, axis=0)
-    #     mean_tpr[-1] = 1.0
-    #     mean_auc = auc(self.mean_fpr, mean_tpr)
-    #     mean_auc_std = float(np.std(self.auc))
-    #
-    #     self.plot_roc(mean_auc, mean_auc_std, mean_tpr)
-
     def save_results_to_csv(self):
         self.results.rename(columns={0: 'Fold Number', 1: 'Training Loss', 2: 'Training Accuracy',
                                      3: 'Validation Loss', 4: 'Validation Accuracy',
@@ -226,4 +205,3 @@ class DataChartIO:
         self.update_values(history, index, tp, fn, fp, tn)
         self.plot_accuracy()
         self.plot_loss()
-        # self.generate_roc_and_auc(validation_labels, validation_predicted_probability)
