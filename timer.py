065a3026d1ce95af85bc4fdfe8f4e7bc74f8d1c2
emcdona1 <emcdona1@neiu.edu>
2020-02-19 17:56:18 -0600

    Smaller methods.

--- timer.py
+++ timer.py
@@ -5,11 +5,18 @@ class Timer:
     def __init__(self, name):
         self.start_time = time.time()
         self.name = name
+        self.duration = None
 
     def stop(self):
         end_time = time.time()
-        print('Completed %s in %.1f seconds' % (self.name, end_time - self.start_time))
+        self.duration = end_time - self.start_time
+
+    def results(self):
+        print('Completed %s in %.1f seconds' % (self.name, self.duration))
+
+    def clear(self):
         self.start_time = None
+        self.duration = None
 
     def start(self, name):
         if not self.start_time:
