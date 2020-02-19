e867a13b9b2de2d050e1eed2a3f928309b5306ce
emcdona1 <emcdona1@neiu.edu>
2020-02-19 17:59:19 -0600

    Cleaning application class.

--- create_models_with_cross_validation.py
+++ create_models_with_cross_validation.py
@@ -6,7 +6,7 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import StratifiedKFold
 import matplotlib
-from image_handling import import_images
+from image_handling import LabeledImages
 from neural_network_models import SmithsonianModel
 from data_and_visualization_io import Charts
 
@@ -20,19 +20,13 @@ random.seed(SEED)
 
 
 def main() -> None:
-    # Set up
-    timer = Timer('TrainingModels')
-    parser = initialize_argparse()
-    args = parser.parse_args()
-    args.color = False if args.bw else True
-    create_folders()
+    timer, args, charts = setup()
 
     # Load in images and shuffle order
-    images = import_images(args.dir, (args.c1, args.c2), args.color, SEED)
+    images = LabeledImages(args.dir, (args.c1, args.c2), args.color, SEED)
 
     # Train model
     skf = StratifiedKFold(n_splits=args.n_folds, shuffle=True, random_state=SEED)
-    charts = Charts()
     for index, (training_idx_list, validation_idx_list) in enumerate(skf.split(images.features, images.labels)):
         # set up training/validation groups
         train_features = images.features[training_idx_list]
@@ -40,14 +34,19 @@ def main() -> None:
         validation_features = images.features[validation_idx_list]
         validation_labels = images.labels[validation_idx_list]
 
-        model_training_results = model_training(index, args, train_features, train_labels,
-                                                validation_features, validation_labels)
-        model_validation(model_training_results, charts, index, args)
-    charts.finalize()
+        architecture = SmithsonianModel(args.img_size, color_mode=args.color, seed=SEED, lr=args.learning_rate)
+        model, history, validation_features, validation_labels = train(index, args,
+                                                                       train_features, train_labels,
+                                                                       validation_features, validation_labels,
+                                                                       architecture)
+        validation_predicted_probability = model.predict_proba(validation_features)[:, 1]
+        charts.update(history, index, validation_labels, validation_predicted_probability, args)
 
+    charts.finalize()
     # end
-    print('c1: ' + args.c1 + ', c2: ' + args.c2)
+    print('class 1: ' + args.c1 + ', class 2: ' + args.c2)
     timer.stop()
+    timer.results()
 
 
 def initialize_argparse() -> argparse.ArgumentParser:
@@ -71,36 +70,32 @@ def initialize_argparse() -> argparse.ArgumentParser:
     return parser
 
 
-def create_folders() -> None:
+def setup():
+    timer = Timer('TrainingModels')
+    parser = initialize_argparse()
+    args = parser.parse_args()
+    args.color = False if args.bw else True
+
+    # create directories
     if not os.path.exists('graphs'):
         os.makedirs('graphs')
     if not os.path.exists('saved_models'):
         os.makedirs('saved_models')
 
+    charts = Charts()
+    return timer, args, charts
 
-def model_training(curr_fold, args, train_features, train_labels, validation_features, validation_labels):
-    architecture = SmithsonianModel(args.img_size, color_mode=args.color, seed=SEED, lr=args.learning_rate)
 
+def train(curr_fold, args, train_features, train_labels, validation_features, validation_labels, architecture):
     print('Training model for fold %i of %i' % (curr_fold + 1, args.n_folds))
     # es_callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', \
     #        mode='min', min_delta = 0.05, patience = 20, restore_best_weights = True)
-    history = architecture.model.fit(train_features, train_labels,
-                                     batch_size=args.batch_size, epochs=args.n_epochs,
+    history = architecture.model.fit(train_features, train_labels, batch_size=args.batch_size, epochs=args.n_epochs,
                                      #        callbacks = [es_callback], \
-                                     validation_data=(validation_features, validation_labels),
-                                     verbose=2)
+                                     validation_data=(validation_features, validation_labels), verbose=2)
     architecture.model.save(os.path.join('saved_models', 'CNN_%i.model' % curr_fold + 1))
 
     return architecture.model, history, validation_features, validation_labels
-
-
-def model_validation(model_training_results, charts, index, args) -> None:
-    model, history, validation_features, validation_labels = model_training_results
-    # Use the model to classify the validation set
-    # Keep only col 1 - prediction probability of class 0
-    validation_predicted_probability = model.predict_proba(validation_features)[:, 1]
-
-    charts.update(history, index, validation_labels, validation_predicted_probability, args)
 
 
 if __name__ == '__main__':
