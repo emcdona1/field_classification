ef0ee04eee37997a9c29cf980a3a4524906b210c
emcdona1 <emcdona1@neiu.edu>
2020-03-06 12:29:15 -0600

    Rename arguments class

--- cnnarguments.py
+++ cnnarguments.py
@@ -2,31 +2,33 @@ import argparse
 import os
 
 
-class Validate:
+class CNNArguments:
     def __init__(self, parser: argparse.ArgumentParser):
         self.parser: argparse.ArgumentParser = parser
 
         # image arguments
-        parser.add_argument('c1', help='Directory name containing images in class 1.')
-        parser.add_argument('c2', help='Directory name containing images in class 2.')
-        parser.add_argument('-s', '--img_size', type=int, default=256,
-                            help='Image dimension in pixels (must be square).')
-        color_mode_group = parser.add_mutually_exclusive_group()
-        color_mode_group.add_argument('-color', action='store_true', help='(default) Images are in RGB color mode.')
+        self.parser.add_argument('c1', help='Directory name containing images in class 1.')
+        self.parser.add_argument('c2', help='Directory name containing images in class 2.')
+        self.parser.add_argument('-s', '--img_size', type=int, default=256,
+                                 help='Image dimension in pixels (must be square). (Default = 256)')
+        color_mode_group = self.parser.add_mutually_exclusive_group()
+        color_mode_group.add_argument('-color', action='store_true', help='Images are in RGB color mode. (Default)')
         color_mode_group.add_argument('-bw', action='store_true', help='Images are in grayscale color mode.')
 
         # model creation argument
-        parser.add_argument('-lr', '--learning_rate', type=float, default=0.0001, help='Learning rate for training.')
+        self.parser.add_argument('-lr', '--learning_rate', type=float,
+                                 default=0.0001, help='Learning rate for training. (Default = 0.0001)')
 
         # training run arguments
-        parser.add_argument('-f', '--n_folds', type=int, default=10,
-                            help='Number of folds (minimum 2) for cross validation.')
-        parser.add_argument('-e', '--n_epochs', type=int, default=25, help='Number of epochs.')
-        parser.add_argument('-b', '--batch_size', type=int, default=64, help='Batch size for training.')
+        self.parser.add_argument('-f', '--n_folds', type=int, default=10,
+                                 help='Number of folds (minimum 2) for cross validation. (Default = 10)')
+        self.parser.add_argument('-e', '--n_epochs', type=int, default=25, help='Number of epochs. (Default = 25)')
+        self.parser.add_argument('-b', '--batch_size', type=int,
+                                 default=64, help='Batch size for training. (Default = 64)')
 
-        self.args: argparse.Namespace = parser.parse_args()
+        self.args: argparse.Namespace = self.parser.parse_args()
 
-    def validate_image_folders(self) -> tuple:
+    def image_folders_and_class_labels(self) -> tuple:
         if not os.path.isdir(self.args.c1):
             raise NotADirectoryError('%s is not a valid directory path.' % self.args.c1)
         if not os.path.isdir(self.args.c2):
@@ -44,35 +46,35 @@ class Validate:
 
         return class1, class2
 
-    def validate_image_size(self) -> int:
+    def image_size(self) -> int:
         img_size = self.args.img_size
         if not img_size >= 4:
             raise ValueError('%i is not a valid image dimension (in pixels). Must be >= 4.' % img_size)
         return img_size
 
-    def validate_learning_rate(self) -> float:
+    def learning_rate(self) -> float:
         lr = self.args.learning_rate
         if not 0 < lr <= 1:
             raise ValueError('%f.6 is not a valid learning rate. Must be in range 0 (exclusive) to 1 (inclusive).' % lr)
         return lr
 
-    def validate_color_mode(self) -> bool:
+    def color_mode(self) -> bool:
         return False if self.args.bw else True
 
-    def validate_n_folds(self) -> int:
+    def n_folds(self) -> int:
         n_folds = self.args.n_folds
         if not n_folds >= 2:
             raise ValueError('%i is not a valid number of folds. Must be >= 2.' % n_folds)
 
         return n_folds
 
-    def validate_n_epochs(self) -> int:
+    def n_epochs(self) -> int:
         n_epochs = self.args.n_epochs
-        # if not n_epochs >= 10:
-        #     raise ValueError('%i is not a valid number of epochs. Must be >= 10.)' % n_epochs)
+        if not n_epochs >= 10:
+            raise ValueError('%i is not a valid number of epochs. Must be >= 10.)' % n_epochs)
         return n_epochs
 
-    def validate_batch_size(self) -> int:
+    def batch_size(self) -> int:
         batch_size = self.args.batch_size
         if not batch_size >= 2:
             raise ValueError('%i is not a valid batch size. Must be >= 2.' % batch_size)
--- create_models_with_cross_validation.py
+++ create_models_with_cross_validation.py
@@ -10,7 +10,7 @@ from data_and_visualization_io import Charts
 from model_training import ModelTrainer
 from timer import Timer
 import warnings
-from validate import Validate
+from cnnarguments import CNNArguments
 
 matplotlib.use('Agg')  # required when running on server
 
@@ -53,14 +53,14 @@ def setup():
 def get_arguments():
     parser = argparse.ArgumentParser(
         'Create and train CNNs for binary classification of images, using cross-fold validation.')
-    val = Validate(parser)
-    image_folders, class_labels = val.validate_image_folders()
-    img_size = val.validate_image_size()
-    lr = val.validate_learning_rate()
-    color_mode = val.validate_color_mode()
-    n_folds = val.validate_n_folds()
-    n_epochs = val.validate_n_epochs()
-    batch_size = val.validate_batch_size()
+    user_arguments = CNNArguments(parser)
+    image_folders, class_labels = user_arguments.image_folders_and_class_labels()
+    img_size = user_arguments.image_size()
+    lr = user_arguments.learning_rate()
+    color_mode = user_arguments.color_mode()
+    n_folds = user_arguments.n_folds()
+    n_epochs = user_arguments.n_epochs()
+    batch_size = user_arguments.batch_size()
     return image_folders, class_labels, img_size, color_mode, lr, n_folds, n_epochs, batch_size
 
 
