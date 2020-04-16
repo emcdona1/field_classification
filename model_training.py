16c36a6cca402a308bdfab4a19bd1461764488c7
emcdona1 <emcdona1@neiu.edu>
2020-04-16 14:34:46 -0500

    If n_folds argument = 1, no cross-fold validation.

--- model_training.py
+++ model_training.py
@@ -23,15 +23,22 @@ class ModelTrainer:
         self.charts = Charts(self.n_folds)
 
     def train_all_models(self, images: LabeledImages):
-        skf = StratifiedKFold(n_splits=self.n_folds, shuffle=True, random_state=self.seed)
-        for index, (training_idx_list, validation_idx_list) in enumerate(skf.split(images.features, images.labels)):
-            self.train_a_model(images, training_idx_list, validation_idx_list, index)
-            self.save_model()
-            self.validate_model(self.charts, images.class_labels)
+        if self.n_folds <= 1:
+            print('Training without cross-fold validation.')
+            # 90% training, 10% validation
+            training_idx_list = np.array(range(int(images.n_images * 0.9)))
+            validation_idx_list = np.array(range(len(training_idx_list), images.n_images))
+            self.train_and_validate_a_model(images, training_idx_list, validation_idx_list, 0)
+        else:
+            print('Training with %i-fold validation.' % self.n_folds)
+            skf = StratifiedKFold(n_splits=self.n_folds, shuffle=True, random_state=self.seed)
+            for index, (training_idx_list, validation_idx_list) in enumerate(skf.split(images.features, images.labels)):
+                self.train_and_validate_a_model(images, training_idx_list, validation_idx_list, index)
         self.charts.finalize()
 
-    def train_a_model(self, images: LabeledImages, training_idx_list: np.ndarray, validation_idx_list: np.ndarray,
-                      curr_fold: int):
+    def train_and_validate_a_model(self, images: LabeledImages, training_idx_list: np.ndarray,
+                                   validation_idx_list: np.ndarray,
+                                   curr_fold: int):
         self.architecture.reset_model()
         self.history = None
         self.training_set = images.subset(training_idx_list)
@@ -45,11 +52,13 @@ class ModelTrainer:
                                                    batch_size=self.batch_size, epochs=self.epochs,
                                                    #        callbacks = [es_callback], \
                                                    validation_data=self.validation_set, verbose=2)
+        self.save_model()
+        self.validate_model(images.class_labels)
 
     def save_model(self):
         self.architecture.model.save(os.path.join(self.folder_name, 'CNN_' + str(self.curr_fold) + '.model'))
 
-    def validate_model(self, charts: Charts, class_labels: tuple) -> None:
+    def validate_model(self, class_labels: tuple) -> None:
         validation_predicted_probability = self.architecture.model.predict_proba(self.validation_set[0])[:, 1]
-        charts.update(self.history, self.curr_fold, self.validation_set[1],
-                      validation_predicted_probability, class_labels)
+        self.charts.update(self.history, self.curr_fold, self.validation_set[1],
+                           validation_predicted_probability, class_labels)
