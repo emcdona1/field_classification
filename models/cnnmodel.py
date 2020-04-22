846d916352e3b45fa66634055f3057f382b65b29
emcdona1 <emcdona1@neiu.edu>
2020-04-22 13:34:01 -0500

    Change loss function (not sparse).

--- models/cnnmodel.py
+++ models/cnnmodel.py
@@ -41,5 +41,6 @@ class CNNModel(ABC):
                                                   decay=0.0,
                                                   amsgrad=False)
         self.model.compile(optimizer=adam_optimizer,
-                           loss='sparse_categorical_crossentropy',
+                           # loss='sparse_categorical_crossentropy',
+                           loss='categorical_crossentropy',
                            metrics=['accuracy'])
