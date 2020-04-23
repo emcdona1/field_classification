6d24b1263d22301301f64613a732744784c807ed
emcdona1 <emcdona1@neiu.edu>
2020-04-22 22:23:44 -0500

    Re-add first hidden layer.

--- models/smithsonian.py
+++ models/smithsonian.py
@@ -26,7 +26,7 @@ class SmithsonianModel(CNNModel):
         # TODO: In Mathematica, Dropout[] has a rate of dropping 50% of elements then * by 2 -- ours does not.
         self.model.add(tf.keras.layers.Dropout(0.5, seed=self.seed))
 
-        # self.model.add(tf.keras.layers.Dense(500, activation="linear"))  # ,
+        self.model.add(tf.keras.layers.Dense(500, activation="linear"))  # ,
         # activity_regularizer=regularizers.l2(0.01),
         # kernel_regularizer=regularizers.l2(0.05)))
 
