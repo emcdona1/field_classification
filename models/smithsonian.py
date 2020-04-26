722b5553d93ed67cbe167bb26639eb01984e44cb
emcdona1 <emcdona1@neiu.edu>
2020-04-26 16:17:22 -0500

    Clean imports.

--- models/smithsonian.py
+++ models/smithsonian.py
@@ -1,5 +1,5 @@
 from models.cnnmodel import CNNModel
-import tensorflow as tf
+from tensorflow.keras import layers
 
 
 class SmithsonianModel(CNNModel):
@@ -7,26 +7,26 @@ class SmithsonianModel(CNNModel):
 
     def add_convolutional_layers(self):
         # Two sets of convolutional layers
-        self.model.add(tf.keras.layers.Conv2D(10, (5, 5), input_shape=(
+        self.model.add(layers.Conv2D(10, (5, 5), input_shape=(
             self.img_dim, self.img_dim, 3)))  # todo: only works for color images
-        self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Activation('relu'))
-        self.model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Activation('relu'))
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-        self.model.add(tf.keras.layers.Conv2D(40, (5, 5)))
-        self.model.add(tf.keras.layers.BatchNormalization())
-        self.model.add(tf.keras.layers.Activation('relu'))
-        self.model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        self.model.add(layers.Conv2D(40, (5, 5)))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Activation('relu'))
+        self.model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
         # Output shape: 40 x 61 x 61
-        self.model.add(tf.keras.layers.Flatten())
+        self.model.add(layers.Flatten())
 
     def add_hidden_layers(self):
-        self.model.add(tf.keras.layers.Dropout(0.5, seed=self.seed))  # drop out 50% and then * 2 (same # of layers)
-        self.model.add(tf.keras.layers.Dense(500, activation='linear'))
-        self.model.add(tf.keras.layers.Dense(500, activation='relu'))
+        self.model.add(layers.Dropout(0.5, seed=self.seed))  # drop out 50% and then * 2 (same # of layers)
+        self.model.add(layers.Dense(500, activation='linear'))
+        self.model.add(layers.Dense(500, activation='relu'))
 
     def add_output_layers(self):
-        self.model.add(tf.keras.layers.Dense(2, activation='linear'))
-        self.model.add(tf.keras.layers.Dense(2, activation="softmax"))
+        self.model.add(layers.Dense(2, activation='linear'))
+        self.model.add(layers.Dense(2, activation="softmax"))
         print(self.model.summary())
