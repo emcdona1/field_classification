e795dfec664aceea12df16230c35f64ef0465e9c
emcdona1 <emcdona1@neiu.edu>
2021-02-01 14:59:47 -0600

    Eliminate redundant code in train_all_models method.

--- model_training.py
+++ model_training.py
@@ -15,7 +15,7 @@ class ModelTrainer:
             os.makedirs(self.folder_name)
         self.architecture = architecture
         self.n_folds = n_folds
-        self.curr_fold = 0
+        self.curr_fold = 1
         self.training_set = None
         self.validation_set = None
         self.history = None
@@ -23,27 +23,36 @@ class ModelTrainer:
         self.charts = Charts(self.n_folds)
 
     def train_all_models(self, images: LabeledImages):
+        training_and_validation_groups = self.generate_image_splits(images)
+
+        for index, (training_idx_list, validation_idx_list) in training_and_validation_groups:
+            self.curr_fold = index + 1
+            self.train_a_model(images, training_idx_list, validation_idx_list)
+            self.save_model()
+            self.validate_model(images.class_labels)
+
+        self.charts.finalize()
+
+    def generate_image_splits(self, images: LabeledImages) -> enumerate:
         if self.n_folds <= 1:
             print('Training without cross-fold validation.')
             # 90% training, 10% validation
             training_idx_list = np.array(range(int(images.n_images * 0.9)))
             validation_idx_list = np.array(range(len(training_idx_list), images.n_images))
-            self.train_and_validate_a_model(images, training_idx_list, validation_idx_list, 0)
+            training_and_validation_groups = enumerate([(training_idx_list, validation_idx_list)])
         else:
             print('Training with %i-fold validation.' % self.n_folds)
             skf = StratifiedKFold(n_splits=self.n_folds, shuffle=True, random_state=self.seed)
-            for index, (training_idx_list, validation_idx_list) in enumerate(skf.split(images.features, images.labels)):
-                self.train_and_validate_a_model(images, training_idx_list, validation_idx_list, index)
-        self.charts.finalize()
+            training_and_validation_groups = enumerate(skf.split(images.features, images.labels))
 
-    def train_and_validate_a_model(self, images: LabeledImages, training_idx_list: np.ndarray,
-                                   validation_idx_list: np.ndarray,
-                                   curr_fold: int):
+        return training_and_validation_groups
+
+    def train_a_model(self, images: LabeledImages, training_idx_list: np.ndarray,
+                      validation_idx_list: np.ndarray):
         self.architecture.reset_model()
         self.history = None
         self.training_set = images.subset(training_idx_list)
         self.validation_set = images.subset(validation_idx_list)
-        self.curr_fold: int = curr_fold + 1
 
         print('Training model for fold %i of %i.' % (self.curr_fold, self.n_folds))
         # es_callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss',
@@ -52,8 +61,6 @@ class ModelTrainer:
                                                    batch_size=self.batch_size, epochs=self.epochs,
                                                    #        callbacks = [es_callback],
                                                    validation_data=self.validation_set, verbose=2)
-        self.save_model()
-        self.validate_model(images.class_labels)
 
     def save_model(self):
         self.architecture.model.save(os.path.join(self.folder_name, 'CNN_' + str(self.curr_fold) + '.model'))
