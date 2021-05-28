a149aa93f0fb826ed1219e988df36a016d10bb42
emcdona1 <emcdona1@neiu.edu>
2021-05-28 10:15:43 -0500

    Methods to go with NewLabeledImage class, plus some typing

--- model_training.py
+++ model_training.py
@@ -1,10 +1,11 @@
 import os
 from models.smithsonian import SmithsonianModel
-from labeled_images.labeledimages import LabeledImages
+from labeled_images.labeledimages import LabeledImages, NewLabeledImages
 import numpy as np
 from data_visualization.visualizationgenerator import VisualizationGenerator
 from sklearn.model_selection import StratifiedKFold
-from tensorflow.keras.models import save_model
+from tensorflow import keras
+from typing import Union, Tuple, List
 
 
 class ModelTrainer:
@@ -23,16 +24,44 @@ class ModelTrainer:
         self.seed: int = seed
         self.charts = VisualizationGenerator(self.n_folds)
 
-    def train_and_save_all_models(self, images: LabeledImages):
-        training_and_validation_groups = self.generate_image_splits(images)
-
-        for index, (training_idx_list, validation_idx_list) in training_and_validation_groups:
+    def new_train_and_save_all_models(self, images: NewLabeledImages):
+        training_and_validation_groups = self.new_generate_image_splits(images)
+        for index, (training_set, validation_set) in enumerate(training_and_validation_groups):
             self.curr_fold = index + 1
-            self.train_model(images, training_idx_list, validation_idx_list)
-            self.save_model()
+            self.training_set = training_set
+            self.validation_set = validation_set
+            self.new_train_model()
+            keras.models.save_model(self.architecture.model,
+                                    os.path.join(self.folder_name, 'CNN_%i.model' % self.curr_fold))
             self.validate_model(images.class_labels)
 
-    def generate_image_splits(self, images: LabeledImages) -> enumerate:
+    def train_and_save_all_models(self, images: LabeledImages):  # todo: remove
+        training_and_validation_groups = self.generate_image_splits(images)
+
+        for index, (training_idx_list, validation_idx_list) in enumerate(training_and_validation_groups):
+            self.curr_fold = index + 1
+            self.train_model(images, training_idx_list, validation_idx_list)
+            keras.models.save_model(self.architecture.model, os.path.join(self.folder_name,
+                                                                          'CNN_' + str(self.curr_fold) + '.model'))
+            self.validate_model(images.class_labels)
+
+    def new_generate_image_splits(self, images: NewLabeledImages):
+        if self.n_folds <= 1:
+            print('Training without cross-fold validation.')
+            # training_idx_list = np.array(range(int(images.img_count * 0.9)))
+            # validation_idx_list = np.array(range(len(training_idx_list), images.img_count))
+            # training_and_validation_groups = [(training_idx_list, validation_idx_list)]
+            # 90% training, 10% validation
+            training_set = images.training_image_set
+            validation_set = images.validation_image_set
+            training_and_validation_groups = [(training_set, validation_set)]
+        else:  # todo: figure out how to do StratifiedKFold with tf.data.Dataset
+            print('Training with %i-fold validation.' % self.n_folds)
+            skf = StratifiedKFold(n_splits=self.n_folds, shuffle=True, random_state=self.seed)
+            training_and_validation_groups = skf.split('features', 'labels')
+        return training_and_validation_groups
+
+    def generate_image_splits(self, images: LabeledImages):  # todo: remove
         if self.n_folds <= 1:
             print('Training without cross-fold validation.')
             # 90% training, 10% validation
@@ -42,9 +71,18 @@ class ModelTrainer:
         else:
             print('Training with %i-fold validation.' % self.n_folds)
             skf = StratifiedKFold(n_splits=self.n_folds, shuffle=True, random_state=self.seed)
-            training_and_validation_groups = enumerate(skf.split(images.features, images.labels))
+            training_and_validation_groups = skf.split(images.features, images.labels)
 
         return training_and_validation_groups
+
+    def new_train_model(self):
+        self.architecture.reset_model()
+        self.history = None
+        print('Training model for fold %i of %i.' % (self.curr_fold, self.n_folds))
+        self.history = self.architecture.model.fit(self.training_set,
+                                                   batch_size=self.batch_size, epochs=self.epochs,
+                                                   #        callbacks = [es_callback],
+                                                   validation_data=self.validation_set, verbose=2)
 
     def train_model(self, images: LabeledImages, training_idx_list: np.ndarray,
                     validation_idx_list: np.ndarray):
@@ -61,11 +99,8 @@ class ModelTrainer:
                                                    #        callbacks = [es_callback],
                                                    validation_data=self.validation_set, verbose=2)
 
-    def save_model(self):
-        save_model(self.architecture.model, os.path.join(self.folder_name, 'CNN_' + str(self.curr_fold) + '.model'))
-
-    def validate_model(self, class_labels: tuple) -> None:
+    def validate_model(self, class_labels: Union[Tuple[str], List[str]]) -> None:
         # validation_predicted_probability = self.architecture.model.predict_proba(self.validation_set[0])[:, 1]
-        validation_predicted_probability = self.architecture.model.predict(self.validation_set[0])[:, 1]
-        self.charts.update(self.history, self.curr_fold, self.validation_set[1],
+        validation_predicted_probability = self.architecture.model.predict(self.validation_set)[:, 1]
+        self.charts.update(self.history, self.curr_fold, self.validation_set[1],  # todo: how to extract the labels from the validation set?
                            validation_predicted_probability, class_labels)
