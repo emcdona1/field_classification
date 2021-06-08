27fde31a5a2bd38632139ef1acc50ac40c671813
emcdona1 <emcdona1@neiu.edu>
2021-06-08 08:58:58 -0500

    Start to add img_names and labels (in progress)

--- labeled_images/labeledimages.py
+++ labeled_images/labeledimages.py
@@ -15,6 +15,8 @@ class LabeledImages:
         self.img_dimensions: tuple = (0, 0)
         self.class_labels: list = []
         self.n_folds: int = 1
+        self.img_names: list = list()
+        self.labels: list = list()
 
     def load_images_from_folders(self, training_images_location: str, image_size: int,
                                  color_mode: ColorMode = ColorMode.rgb, shuffle=True, n_folds=1, batch_size=32) -> None:
@@ -46,12 +48,26 @@ class LabeledImages:
         if shuffle:
             self.shuffle_images()
 
-    def load_testing_images(self, testing_image_folder: str):
-        # todo: implement using this in testing script
+    def load_testing_images(self, testing_image_folder: str, image_size: int, color_mode: ColorMode = ColorMode.rgb):
+        self.color_mode = color_mode
+        self.img_dimensions = (image_size, image_size)
         self.test_image_set = tf.keras.preprocessing.image_dataset_from_directory(testing_image_folder,
                                                                                   color_mode=self.color_mode.name,
-                                                                                  image_size=self.img_dimensions)
+                                                                                  image_size=self.img_dimensions,
+                                                                                  seed=self.seed,
+                                                                                  batch_size=32)
+        self.class_labels = self.test_image_set.class_names
         self.test_image_set = self.test_image_set.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
+
+        # todo: img_names and labels aren't in the same order!!!!
+        self.img_names = list()
+        folders = os.listdir(testing_image_folder)
+        for folder in folders:
+            for f in self.test_image_set.list_files(os.path.join('.', testing_image_folder, folder, '*.jpg'))\
+                    .as_numpy_iterator():
+                self.img_names.append(f)
+        self.labels = list(list(self.test_image_set.as_numpy_iterator())[0][1])
+
 
     def shuffle_images(self):
         # todo: shuffle images in train/validation sets
