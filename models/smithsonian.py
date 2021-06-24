189e61ec395160c6c5c73621bf10498a7245d7ae
emcdona1 <emcdona1@neiu.edu>
2021-06-24 14:41:15 -0500

    More compact code.

--- models/smithsonian.py
+++ models/smithsonian.py
@@ -8,13 +8,10 @@ class SmithsonianModel(CNNModel):
     """ Creates the model architecture as outlined in Schuettpelz, Frandsen, Dikow, Brown, et al. (2017). """
 
     def add_convolutional_layers(self):
+        channels = 3 if self.color == ColorMode.rgb else 1
+
         # Two sets of convolutional layers
-        if self.color == ColorMode.rgb:
-            print('Color image input layer')
-            self.model.add(layers.Conv2D(10, (5, 5), input_shape=(self.img_dim, self.img_dim, 3)))
-        else:
-            print('Grayscale image input layer')
-            self.model.add(layers.Conv2D(10, (5, 5), input_shape=(self.img_dim, self.img_dim, 1)))
+        self.model.add(layers.Conv2D(10, (5, 5), input_shape=(self.img_dim, self.img_dim, channels)))
         self.model.add(layers.BatchNormalization())
         self.model.add(layers.Activation('relu'))
         self.model.add(layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
@@ -26,9 +23,9 @@ class SmithsonianModel(CNNModel):
 
         # Output shape: 40 x 61 x 61
         self.model.add(layers.Flatten())
+        self.model.add(layers.Dropout(0.5, seed=self.seed))  # drop out 50% and then * 2 (same # of layers)
 
     def add_hidden_layers(self):
-        self.model.add(layers.Dropout(0.5, seed=self.seed))  # drop out 50% and then * 2 (same # of layers)
         self.model.add(layers.Dense(500, activation='linear'))
         self.model.add(layers.Dense(500, activation='relu'))
 
