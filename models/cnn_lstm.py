bfc9ed0d6f3328d016095b3e2758414f25d9b7c5
emcdona1 <emcdona1@neiu.edu>
2021-07-27 10:27:06 -0500

    Clean up imports/calls (add keras to imports)

--- models/cnn_lstm.py
+++ models/cnn_lstm.py
@@ -2,6 +2,7 @@
 # https://github.com/githubharald/SimpleHTR
 # https://stackoverflow.com/questions/63258412/adding-ctc-loss-and-ctc-decode-to-a-keras-model#63306211
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import layers
 from models.cnnmodel import CNNModel
 from labeled_images.colormode import ColorMode
@@ -9,15 +10,15 @@ from labeled_images.colormode import ColorMode
 
 class CnnLstm(CNNModel):
     def layer_setup(self):
-        model_input = tf.keras.Input(shape=(800, 64, 1))  # todo: not hard-coding the shape? or preprocess?
-        self.model = tf.keras.layers.experimental.preprocessing.Rescaling(1 / 255)(model_input)
+        self.inputs = keras.Input(shape=(800, 64, 1))  # todo: not hard-coding the shape? or preprocess?
+        self.model = keras.layers.experimental.preprocessing.Rescaling(1 / 255)(self.inputs)
         self.add_convolutional_layers()
         self.add_rnn_layers()
-        # self.add_hidden_layers()  # actually CTC
-        output = tf.keras.layers.Dense(10)(self.model)
-        model_tmp = tf.keras.Model(inputs=model_input, outputs=output, name='tmp')
-        model_tmp.summary()
-        # self.add_output_layers() todo - and rename to better match the functionality
+        self.add_output_layers()
+        self.outputs = self.model
+        self.testing_model = keras.Model(inputs=self.inputs, outputs=self.outputs, name='testing_model')
+        self.testing_model.summary()
+        self.model = None
         # self.compile_model()
 
     def add_convolutional_layers(self):
@@ -32,34 +33,34 @@ class CnnLstm(CNNModel):
         # create layers
         for i in range(num_layers):
             if i == 3:
-                self.model = tf.keras.layers.Conv2D(filters=256,
+                self.model = layers.Conv2D(filters=256,
                                                       kernel_size=(kernel_values[i], kernel_values[i]),
                                                       padding='SAME',
                                                       activation='relu')(self.model)
-            set_of_layers = tf.keras.Sequential()
+            set_of_layers = keras.Sequential()
             set_of_layers.add(
-                tf.keras.layers.Conv2D(filters=feature_values[i],
+                layers.Conv2D(filters=feature_values[i],
                                        kernel_size=(kernel_values[i], kernel_values[i]),
                                        padding='SAME',
                                        activation='relu'))
-            set_of_layers.add(tf.keras.layers.BatchNormalization())
-            set_of_layers.add(tf.keras.layers.MaxPooling2D(pool_size=(pool_values[i][0], pool_values[i][1]),
+            set_of_layers.add(layers.BatchNormalization())
+            set_of_layers.add(layers.MaxPooling2D(pool_size=(pool_values[i][0], pool_values[i][1]),
                                                            strides=(stride_values[i][0], stride_values[i][1]),
                                                            padding='VALID'))
             self.model = set_of_layers(self.model)
 
     def add_rnn_layers(self):
         """Create RNN layers."""
-        self.model = tf.keras.layers.Lambda(lambda t: t[:, :, 0, :])(self.model)
+        self.model = layers.Lambda(lambda t: t[:, :, 0, :])(self.model)
         # rnn_in3d = tf.squeeze(self.cnn_out_4d, axis=[2])  # todo
 
         # basic cells which is used to build RNN
         num_hidden = 512
 
-        rnn = [tf.keras.layers.LSTMCell(num_hidden), tf.keras.layers.LSTMCell(num_hidden)]
-        rnn = tf.keras.layers.StackedRNNCells(rnn)
-        rnn = tf.keras.layers.RNN(rnn, return_sequences=True)
-        self.model = tf.keras.layers.Bidirectional(rnn)(self.model)
+        rnn = [layers.LSTMCell(num_hidden), layers.LSTMCell(num_hidden)]
+        rnn = layers.StackedRNNCells(rnn)
+        rnn = layers.RNN(rnn, return_sequences=True)
+        self.model = layers.Bidirectional(rnn)(self.model)
 
     def add_hidden_layers(self):
         pass
