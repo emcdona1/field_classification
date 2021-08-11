e3eaac2bfc23e1db72f45c5aa821cad3adf35ca6
emcdona1 <emcdona1@neiu.edu>
2021-08-11 12:00:52 -0500

    Updates to current ModelTrainer (validate_ takes in whole LabeledImages object)

--- models/model_training.py
+++ models/model_training.py
@@ -1,10 +1,10 @@
 import os
+import pandas as pd
 from models import CNNModel
 from labeled_images.labeledimages import LabeledImages
 import numpy as np
 from data_visualization.visualizationgenerator import VisualizationGenerator
 from tensorflow import keras
-from typing import Union, Tuple, List
 import tensorflow as tf
 
 
@@ -23,14 +23,13 @@ class ModelTrainer:
         self.charts = VisualizationGenerator(self.n_folds)
 
     def train_and_save_all_models(self, images: LabeledImages):
-        # training_groups, validation_groups = self.generate_image_splits(images)
         fold_groups = zip(images.training_image_set, images.validation_image_set)
         for index, (training_set, validation_set) in enumerate(fold_groups):
             self.curr_index = index
             self.train_model(training_set, validation_set)
             keras.models.save_model(self.architecture.model,
                                     os.path.join(self.folder_name, f'CNN_{self.curr_index + 1}.model'))
-            self.validate_model_at_epoch_end(images.class_labels, validation_set)
+            self.validate_model_at_epoch_end(images, validation_set)
 
     def train_model(self, training_set: tf.data.Dataset, validation_set: tf.data.Dataset) -> None:
         self.architecture.reset_model()
@@ -42,13 +41,12 @@ class ModelTrainer:
                                                   verbose=2)
         self.history.append(new_history)
 
-    def validate_model_at_epoch_end(self, class_labels: Union[Tuple[str], List[str]],
-                                    validation_set: tf.data.Dataset) -> None:
-        # todo: possibly change this into model.evaluate?
-        validation_predicted_probability = self.architecture.model.predict(validation_set)[:, 1]  # ,0]
+    def validate_model_at_epoch_end(self, images: LabeledImages, validation_set: tf.data.Dataset) -> None:
         validation_labels = np.array([])
+        validation_predicted_probability = self.architecture.model.predict(validation_set)[:, 1]  # ,0]
         for batch in validation_set.as_numpy_iterator():
             validation_labels = np.concatenate((validation_labels, batch[1]))
         validation_labels = validation_labels.astype(np.int32)
         self.charts.update(self.history[self.curr_index], self.curr_index + 1, validation_labels,
-                           validation_predicted_probability, class_labels)
+                           validation_predicted_probability, images.class_labels)
+
