3e6ae243498825110fce5047d1438b9b4a449f1e
emcdona1 <emcdona1@neiu.edu>
2021-08-11 12:01:32 -0500

    Create child class for CtcModelTrainer (mainly, validation happens differently and no graphs are saved).

--- models/model_training.py
+++ models/model_training.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from models import CNNModel
-from labeled_images.labeledimages import LabeledImages
+from labeled_images.labeledimages import LabeledImages, MAX_LABEL_LENGTH, num_to_char
 import numpy as np
 from data_visualization.visualizationgenerator import VisualizationGenerator
 from tensorflow import keras
@@ -50,3 +50,33 @@ class ModelTrainer:
         self.charts.update(self.history[self.curr_index], self.curr_index + 1, validation_labels,
                            validation_predicted_probability, images.class_labels)
 
+
+def decode_batch_predictions(predictions: np.ndarray):
+    input_len = np.ones(predictions.shape[0]) * predictions.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search
+    results = keras.backend.ctc_decode(predictions, input_length=input_len, greedy=True)[0][0][:, :MAX_LABEL_LENGTH]
+    # Iterate over the results and get back the text
+    output_text = []
+    for res in results:
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8")
+        res = res.replace('[UNK]', '').replace(' ', '')
+        output_text.append(res)
+    return output_text
+
+
+class CtcModelTrainer(ModelTrainer):
+    def validate_model_at_epoch_end(self, images: LabeledImages, validation_set: tf.data.Dataset) -> None:
+        predictions = pd.DataFrame(columns=['actual', 'predicted'])
+        for batch in validation_set.as_numpy_iterator():
+            validation_predicted_probability = self.architecture.model.predict(batch)
+            predicted_labels = decode_batch_predictions(validation_predicted_probability)
+            actual_labels_as_byte_lists = num_to_char(batch['label']).numpy()
+            actual_labels = list()
+            for row in actual_labels_as_byte_lists:
+                row = [c.decode('utf-8') for c in row]
+                row = ''.join(row).strip()
+                actual_labels.append(row)
+            batch_predictions = pd.DataFrame({'actual': actual_labels, 'predicted': predicted_labels})
+            predictions = predictions.append(batch_predictions, ignore_index=True)
+        print(predictions)
+        # todo: output these results somehow
