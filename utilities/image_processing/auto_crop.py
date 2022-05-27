8826096dfd63404a8cf7197789432b7495ccf6aa
emcdona1 <emcdona1@neiu.edu>
2022-05-27 10:39:20 -0500

    Readability, tighten scope, use Path and f-strings

--- utilities/image_processing/auto_crop.py
+++ utilities/image_processing/auto_crop.py
@@ -23,29 +23,22 @@ def main(image_filename: Path, image_save_path: Path):
     thresholded_image = image.imread(thresholded_image_save_location)
     segment_image(thresholded_image, image_filename, image_save_path)
 
-def segment_image(img):
 
-    # apply threshold
-    thresh = threshold_otsu(img)
-    bw = closing(img > thresh, square(5))
-
-    # remove artifacts connected to image border
-    cleared = clear_border(bw)
-
-    # label image regions
-    label_image = label(cleared)
+def segment_image(img, image_filename: Path, image_save_path: Path):
+    thresh = filters.threshold_otsu(img)
+    bw = morphology.closing(img > thresh, morphology.square(5))
+    cleared = segmentation.clear_border(bw)
+    label_image = measure.label(cleared)
 
     # to make the background transparent, pass the value of `bg_label`,
     # and leave `bg_color` as `None` and `kind` as `overlay`
-    image_label_overlay = label2rgb(label_image, image=img, bg_label=0)
+    image_label_overlay = color.label2rgb(label_image, image=img, bg_label=0)
 
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.imshow(image_label_overlay)
 
-    for region in regionprops(label_image):
-        # take regions with large enough areas
+    for region in measure.regionprops(label_image):
         if region.area >= 8000:
-
             # Set the length and height of each box to help filer out unwanted segments
             minr, minc, maxr, maxc = region.bbox
             length = (maxr - minr)
@@ -54,10 +47,9 @@ def segment_image(img):
             # Only draw boxes around segments to the right 1000 pixels and that are wider than long
             # This helps us not segment out parts of the ruler in or image that mess with our data
             if minc > 1000 and length < width:
-
                 # draw rectangle around segmented leaves
                 rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                      fill=False, edgecolor='red', linewidth=2)
+                                          fill=False, edgecolor='red', linewidth=2)
                 ax.add_patch(rect)
 
                 lst.append(minc)
@@ -65,18 +57,15 @@ def segment_image(img):
                 lst.append(maxc)
                 lst.append(maxr)
 
-    # Show image with regions outlined
     ax.set_axis_off()
     plt.tight_layout()
     plt.show()
 
-    # Execute the crop function to crop the highlighted regions
-    crop()
+    crop(image_filename, image_save_path)
 
 
-def crop():
+def crop(image_filename: Path, image_save_path: Path):
     global count
-
     count = 0
     runs = 0
 
@@ -89,90 +78,48 @@ def crop():
 
             # Open original image and crop using our
             # coordinates we selected with slight padding
-            img_crop = Image.open(filename)
+            img_crop = Image.open(image_filename)
             img_crop_res = img_crop.crop((x1 - 10, x2 - 10, y1 + 10, y2 + 10))
 
-            # Split our filename to get the desired sections
-            # and exclude the .jpg or .png extension
-            fn_split = filename.split("/")
-            species = fn_split[-2]
-            file = fn_split[-1].replace('.jpg', '')
-            file = file.replace('.png', '')
+            species = image_filename.parent.stem
+            file = image_filename.stem
 
-            # Merge back together our file name excluding the
-            # parts we do not want
-            a = '/'
-            path = fn_split[:-1]
-            path = a.join(path)
-            print(path)
-
-            # Increment count and runs
             count += 4
             runs += 1
 
-            # Save cropped image and cropped image file name
-            saved_file = species + '_' + file + '_' + 'AUTOCROP' + '_' + str(runs) + '.jpg'
-            img_crop_res.save(path + "/" + species + '_' + file + '_' + 'AUTOCROP' + '_' + str(runs) + '.jpg')
+            saved_file = f'{species}_{file}_AUTOCROP_{runs}.jpg'
+            img_crop_res.save(Path(image_save_path, saved_file))
+            print(f'Saved new image: {saved_file}.')
 
-            # Print the file name and where it was saved
-            print('Saved File: ' + saved_file + ' to ' + path)
-
-            # convert image object into array
-            imageinfo = numpy.asarray(img_crop_res)
-
-            # Store the image height and width and new file location
+            imageinfo = np.asarray(img_crop_res)
             height = imageinfo.shape[0]
             width = imageinfo.shape[1]
 
-            # Save new file location
-            file_location = path + "/" + saved_file
-
-            # open existing CSV file and store img height, width, and file path
-            with open('auto_crop_image_data.csv', 'a') as f:
+            file_location = Path(image_save_path, saved_file)
+            with open(Path(image_save_path, 'auto_crop_image_data.csv'), 'a') as f:
                 writer = csv.writer(f)
                 writer.writerow(([height, width, file_location]))
-                f.close()
 
-            # Print to terminal to confirm file is saved
-            print(saved_file + " has been written to cropped_image_data.csv")
-
-
-# Select file and get file name
-def upload_action(event=None):
-    filename = filedialog.askopenfilename()
-    return filename
-
-
-# Threshold the image to be segmented
-def img_thresh():
-
-    # Load selected image
-    image = cv2.imread(filename)
-
-    # Convert image to grayscale
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # all pixels value above 200 will be set to 255
-    ret, thresh2 = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY_INV)
 
 def img_thresh(image_filename: Path, image_save_path: Path) -> Path:
     img = cv2.imread(str(image_filename), cv2.IMREAD_GRAYSCALE)
     ret, thresh2 = cv2.threshold(img, BACKGROUND_COLOR_THRESHOLD, 255, cv2.THRESH_BINARY_INV)
 
-    # Merge back together our file name excluding the
-    # parts we do not want
-    a = '/'
-    path = fn_split[:-1]
-    path = a.join(path)
+    species = image_filename.parent.stem
+    file = image_filename.stem
+    tmp_folder = Path(image_save_path, 'tmp')
+    if not os.path.exists(tmp_folder):
+        os.makedirs(tmp_folder)
 
-    # Save thresholded image and image name
-    cv2.imwrite(path + "/" + species + '_' + file + '_' + 'THRESH' + '.jpg', thresh2)
-    imagename = path + "/" + species + '_' + file + '_' + 'THRESH' + '.jpg'
-
-    print(imagename)
-    return imagename
+    image_name = f'{species}_{file}_THRESH.jpg'
+    threshold_save_path = Path(tmp_folder, image_name)
+    cv2.imwrite(str(threshold_save_path), thresh2)
+    return threshold_save_path
 
 
 if __name__ == '__main__':
-    filename = upload_action()
-    main()
+    filename = Path(filedialog.askopenfilename())
+    save_path = Path(filename.parent, 'auto_cropped_images')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    main(filename, save_path)
