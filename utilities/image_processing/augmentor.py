10357096ed6216f5005ea5bf7582c414479a7ba2
emcdona1 <emcdona1@neiu.edu>
2022-05-27 09:55:12 -0500

    Clarify comments

--- utilities/image_processing/augmentor.py
+++ utilities/image_processing/augmentor.py
@@ -10,23 +10,23 @@ def main(training_image_source_folder: Path, training_batch_size: int,
     for folder, batch_size in image_folders:
         augmentation_pipeline = Augmentor.Pipeline(folder)
 
-        # aug1
+        # Augmentation option #1
         augmentation_pipeline.rotate(probability=1.0, max_left_rotation=15, max_right_rotation=15)
         augmentation_pipeline.flip_random(probability=1.0)
 
-        # aug2
+        # Augmentation option #2
         # augmentation_pipeline.rotate(probability=.5, max_left_rotation=15, max_right_rotation=15)
         # augmentation_pipeline.rotate180(probability=.5)
         # augmentation_pipeline.flip_random(probability=.5)
         # augmentation_pipeline.shear(probability=.5, max_shear_left=10, max_shear_right=10)
         # augmentation_pipeline.skew(probability=.5)
 
-        # aug3
+        # Augmentation option #3
         # augmentation_pipeline.rotate(probability=.5, max_left_rotation=20, max_right_rotation=20)
         # augmentation_pipeline.rotate180(probability=.5)
         # augmentation_pipeline.flip_random(probability=1)
 
-        # aug4
+        # Augmentation option #4
         # augmentation_pipeline.rotate(probability=1, max_right_rotation=20, max_left_rotation=20)
         # augmentation_pipeline.flip_random(probability=1)
         # augmentation_pipeline.crop_random(probability=1, percentage_area=.9)
