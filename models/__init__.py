86053988ef3035a3cdf7ba9d419634847e70520f
emcdona1 <emcdona1@neiu.edu>
2022-05-26 22:52:53 -0500

    Remove old transfer learning model

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,2 @@
 from models.cnnmodel import CNNModel
 from models.smithsonian import SmithsonianModel
-from models.transfer_learning_model import TransferLearningModel
-from models.rnn_ctc import RnnCtc
