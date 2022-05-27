dc12636c1d323ac8fd488e2b5d068bfe475b58d7
emcdona1 <emcdona1@neiu.edu>
2022-05-27 09:44:13 -0500

    Accept Path or str for file system locations, return Path objects

--- utilities/dataloader.py
+++ utilities/dataloader.py
@@ -10,7 +10,7 @@ import pandas as pd
 from urllib.request import urlopen, Request
 
 
-def load_list_from_txt(file_path: str) -> list:
+def load_list_from_txt(file_path: Union[str, Path]) -> list:
     results = list()
     with open(file_path, 'r') as f:
         lines = f.readlines()
@@ -22,8 +22,9 @@ def load_list_from_txt(file_path: str) -> list:
 
 def load_file_list_from_filesystem(directory_or_file: Union[str, Path]) -> List[Path]:
     if os.path.isdir(directory_or_file):
+        directory_or_file = Path(directory_or_file)
         all_directory_contents = os.listdir(directory_or_file)
-        all_directory_contents_with_full_path = [Path(os.path.join(directory_or_file, filename))
+        all_directory_contents_with_full_path = [Path(directory_or_file, filename)
                                                  for filename in all_directory_contents]
         results = [item for item in all_directory_contents_with_full_path if not os.path.isdir(item)]
     elif os.path.isfile(directory_or_file):
@@ -34,25 +35,25 @@ def load_file_list_from_filesystem(directory_or_file: Union[str, Path]) -> List[
     return results
 
 
-def load_pickle(pickle_file_path: str):
+def load_pickle(pickle_file_path: Union[Path, str]):
     with open(pickle_file_path, 'rb') as file:
         de_pickled = pickle.load(file)
     return de_pickled
 
 
-def pickle_an_object(save_directory: str, object_id: str, obj_to_pickle) -> str:
+def pickle_an_object(save_directory: Union[Path, str], object_id: str, obj_to_pickle: any) -> Path:
     if not os.path.exists(save_directory):
         os.makedirs(save_directory)
     filename = object_id + '.pickle'
-    file_path = os.path.join(save_directory, filename)
+    file_path = Path(save_directory, filename)
     with open(file_path, 'wb') as file:
         pickle.dump(obj_to_pickle, file)
     return file_path
 
 
-def open_cv2_image(image_location: Union[str, Path, Request], rgb: bool = True) -> np.ndarray:
-    color_mode = cv2.IMREAD_COLOR if rgb else cv2.IMREAD_GRAYSCALE
-    if (type(image_location) is str and 'http' in image_location) or type(image_location) is Request:
+def open_cv2_image(image_location: Union[str, Path, Request], is_rgb_image: bool = True) -> np.ndarray:
+    color_mode = cv2.IMREAD_COLOR if is_rgb_image else cv2.IMREAD_GRAYSCALE
+    if type(image_location) is Request or (type(image_location) is str and 'http' in image_location):
         resp = urlopen(image_location)
         image = np.asarray(bytearray(resp.read()), dtype='uint8')
         image_to_draw_on = cv2.imdecode(image, color_mode)
@@ -72,15 +73,14 @@ def get_timestamp_for_file_saving() -> str:
     return datetime.strftime(datetime.now(), '%Y_%m_%d-%H_%M_%S')
 
 
-def save_dataframe_as_csv(save_location: str, file_id: str, df: pd.DataFrame, timestamp=True) -> str:
-    file_location = os.path.join(save_location, file_id +
-                                 (('-' + get_timestamp_for_file_saving()) if timestamp else '') + '.csv')
+def save_dataframe_as_csv(save_location: Union[Path, str], file_id: str, df: pd.DataFrame, timestamp=True) -> Path:
+    file_location = Path(save_location, f'{file_id}{"-" + get_timestamp_for_file_saving() if timestamp else ""}.csv')
     df.to_csv(file_location, index=False, encoding='UTF-8')
     return file_location
 
 
-def download_image(image_url: str, save_directory: str, image_id: str) -> str:
-    image_save_path = os.path.join(save_directory, image_id + '.jpg')
+def download_image(image_url: str, save_directory: Union[Path, str], image_id: str) -> Path:
+    image_save_path = Path(save_directory, f'{image_id}.jpg')
     if os.path.exists(image_save_path):
         image_save_path = 'ALREADY_EXISTS'
     else:
