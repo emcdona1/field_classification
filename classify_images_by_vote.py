d5040bade1d19f6927f3884179a18fd4a742fd6c
emcdona1 <emcdona1@neiu.edu>
2022-06-10 16:55:45 -0500

    Pull arguments out to modeltrainingarguments.py

--- classify_images_by_vote.py
+++ classify_images_by_vote.py
@@ -9,7 +9,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDis
 import tensorflow as tf
 from utilities.timer import Timer
 from labeled_images.labeledimages import LabeledTestingImages
-from labeled_images.colormode import ColorMode
+from models.modeltrainingarguments import ModelTestingArguments
 
 THRESHOLD = 0.5
 SEED = 1
@@ -17,30 +17,30 @@ SEED = 1
 
 def main():
     timer = Timer('Classify test images')
-    image_folders, list_of_models, color_mode, image_size = process_input_arguments()
+    arguments = ModelTestingArguments()
 
     images = LabeledTestingImages(SEED)
-    images.load_testing_images(image_folders, image_size, color_mode)
+    images.load_testing_images(arguments.image_folders, arguments.image_size, arguments.color_mode)
     print('Images imported.')
 
     results_dir = 'test_results'
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
 
-    if len(list_of_models) == 1:
-        predictions = predict_test_images(images, list_of_models[0])
+    if len(arguments.list_of_models) == 1:
+        predictions = predict_test_images(images, arguments.list_of_models[0])
         predicted_class = np.argmax(np.array(predictions), axis=1)
 
         combined_results = pd.DataFrame()
         combined_results['image filenames'] = images.test_image_file_paths
         combined_results['actual_class'] = images.test_labels
         combined_results['voted_label'] = predicted_class
-        combined_results[(predictions.columns)] = predictions
+        combined_results[predictions.columns] = predictions
         generate_confusion_matrix(len(images.class_labels), images, predicted_class, results_dir)
     else:
         predictions = list()
         predicted_class = list()
-        for model_path in list_of_models:
+        for model_path in arguments.list_of_models:
             next_predictions, num_classes = predict_test_images(images, model_path)
             predictions.append(next_predictions)
             predicted_class.append(np.argmax(np.array(predictions), axis=1))
@@ -81,28 +81,6 @@ def predict_test_images(images: LabeledTestingImages, model_path: Union[str, Pat
         return predictions
     else:
         print(f'Model file path error: {model_path} is not a *.model file.')  # todo move this to argument processing
-
-
-def process_input_arguments():
-    parser = argparse.ArgumentParser('Import a model and classify images.')
-    parser.add_argument('images', help='Path containing folders of test images')
-    parser.add_argument('img_size', type=int, help='Image dimension (one side in pixels -- square image assumed).')
-    parser.add_argument('models', help='One model, or one folder of models to use.')
-    color_mode_group = parser.add_mutually_exclusive_group()
-    color_mode_group.add_argument('-color', action='store_true', help='Images are in RGB color mode. (Default)')
-    color_mode_group.add_argument('-bw', action='store_true', help='Images are in grayscale color mode.')
-    args = parser.parse_args()
-
-    image_folders = args.images
-    if '.model' in args.models:
-        list_of_models = [Path(args.models)]
-    else:
-        list_of_models = os.listdir(args.models)
-        list_of_models = [Path(args.models, filename) for filename in list_of_models]
-    color_mode = ColorMode.grayscale if args.bw else ColorMode.rgb
-    image_size = (args.img_size, args.img_size)
-
-    return image_folders, list_of_models, color_mode, image_size
 
 
 if __name__ == '__main__':
