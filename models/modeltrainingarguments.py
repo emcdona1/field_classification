b106e63f538f2f43637002d6c00ec6ee325cc1d0
emcdona1 <emcdona1@neiu.edu>
2022-06-10 16:56:15 -0500

    Create abstract base class, then implement test arguments child class

--- models/modeltrainingarguments.py
+++ models/modeltrainingarguments.py
@@ -1,48 +1,23 @@
+from abc import abstractmethod, ABC
 import argparse
 import os
 from labeled_images.colormode import ColorMode
 from pathlib import Path
 from typing import Tuple
 
-
-class ModelTrainingArguments:
+class ModelArguments(ABC):
     def __init__(self):
         self._parser = argparse.ArgumentParser('Create and train NNs for image classification.')
         self._args: argparse.Namespace = self.set_up_parser_arguments()
-
-        self.training_image_folder: Path = self._validate_training_folder()
         self.image_size: Tuple[int, int] = self._validate_image_size()
         self.color_mode: ColorMode = self._set_color_mode()
-        self.lr: float = self._validate_learning_rate()
-        self.n_epochs: int = self._validate_n_epochs()
-        self.batch_size: int = self._validate_batch_size()
 
-        self.num_output_classes: int = self._validate_num_output_classes()
-
+    @abstractmethod
     def set_up_parser_arguments(self):
-        self._parser.add_argument('training_set', help='Directory containing training images.')
+        pass
 
-        self._parser.add_argument('height', type=int, default=256, help='Desired image height.')
-        self._parser.add_argument('-w', '--width', type=int, help='Desired image width. (Omit for square images)')
-        color_mode_group = self._parser.add_mutually_exclusive_group()
-        color_mode_group.add_argument('-color', action='store_true', help='Images are in RGB color mode. (Default)')
-        color_mode_group.add_argument('-bw', action='store_true', help='Images are in grayscale color mode.')
-
-        self._parser.add_argument('-lr', '--learning_rate', type=float,
-                                  default=0.001, help='Learning rate for training. (Default = 0.001)')
-        self._parser.add_argument('-e', '--n_epochs', type=int, default=25,
-                                  help='Number of epochs (minimum 5) per fold. (Default = 25)')
-        self._parser.add_argument('-b', '--batch_size', type=int,
-                                  default=64, help='Batch size (minimum 2) for training. (Default = 64)')
-        self._parser.add_argument('-cls', '--num_output_classes', default=2, type=int,
-                                  help='Class size minimum 2, no max. (Default = 2)')
-
-        return self._parser.parse_args()
-
-    def _validate_training_folder(self) -> Path:
-        training_path = Path(self._args.training_set)
-        assert os.path.isdir(training_path), f'{self._args.training_set} is not a valid directory path.'
-        return training_path
+    def _set_color_mode(self) -> ColorMode:
+        return ColorMode.grayscale if self._args.bw else ColorMode.rgb
 
     def _validate_image_size(self) -> (int, int):
         assert self._args.height > 0, f'Image height must be > 0. {self._args.height} is not valid.'
@@ -52,8 +27,37 @@ class ModelTrainingArguments:
             width = self._args.width
         return height, width
 
-    def _set_color_mode(self) -> ColorMode:
-        return ColorMode.grayscale if self._args.bw else ColorMode.rgb
+    def _validate_image_folder(self, folder: str) -> Path:
+        image_folder_path = Path(folder)
+        assert os.path.isdir(image_folder_path), f'{folder} is not a valid directory path.'
+        return image_folder_path
+
+
+class ModelTrainingArguments(ModelArguments):
+    def __init__(self):
+        super().__init__()
+        self.training_image_folder: Path = self._validate_image_folder(self._args.training_set)
+        self.lr: float = self._validate_learning_rate()
+        self.n_epochs: int = self._validate_n_epochs()
+        self.batch_size: int = self._validate_batch_size()
+        self.num_output_classes: int = self._validate_num_output_classes()
+
+    def set_up_parser_arguments(self):
+        self._parser.add_argument('training_set', help='Directory containing training images.')
+        self._parser.add_argument('height', type=int, default=256, help='Desired image height.')
+        self._parser.add_argument('-w', '--width', type=int, help='Desired image width. (Omit for square images)')
+        color_mode_group = self._parser.add_mutually_exclusive_group()
+        color_mode_group.add_argument('-color', action='store_true', help='Images are in RGB color mode. (Default)')
+        color_mode_group.add_argument('-bw', action='store_true', help='Images are in grayscale color mode.')
+        self._parser.add_argument('-lr', '--learning_rate', type=float,
+                                  default=0.001, help='Learning rate for training. (Default = 0.001)')
+        self._parser.add_argument('-e', '--n_epochs', type=int, default=25,
+                                  help='Number of epochs (minimum 5) per fold. (Default = 25)')
+        self._parser.add_argument('-b', '--batch_size', type=int,
+                                  default=64, help='Batch size (minimum 2) for training. (Default = 64)')
+        self._parser.add_argument('-cls', '--num_output_classes', default=2, type=int,
+                                  help='Class size minimum 2, no max. (Default = 2)')
+        return self._parser.parse_args()
 
     def _validate_learning_rate(self) -> float:
         lr = self._args.learning_rate
@@ -74,3 +78,29 @@ class ModelTrainingArguments:
         num_output_classes = self._args.num_output_classes
         assert num_output_classes >= 2, f'{num_output_classes} is not a valid number of classes. Must be >= 2.'
         return num_output_classes
+
+
+class ModelTestingArguments(ModelArguments):
+    def __init__(self):
+        super().__init__()
+        self.image_folders = self._validate_image_folder(self._args.images)
+        self.list_of_models = self._validate_models()
+        self.image_size = self._validate_image_size()
+
+    def set_up_parser_arguments(self):
+        self._parser.add_argument('images', help='Folder containing test images.')
+        self._parser.add_argument('models', help='One model, or one folder of models to use.')
+        self._parser.add_argument('height', type=int, default=256, help='Desired image height.')
+        self._parser.add_argument('-w', '--width', type=int, help='Desired image width. (Omit for square images)')
+        color_mode_group = self._parser.add_mutually_exclusive_group()
+        color_mode_group.add_argument('-color', action='store_true', help='Images are in RGB color mode. (Default)')
+        color_mode_group.add_argument('-bw', action='store_true', help='Images are in grayscale color mode.')
+        return self._parser.parse_args()
+
+    def _validate_models(self):
+        if '.model' in self._args.models:
+            list_of_models = [Path(self._args.models)]
+        else:
+            list_of_models = os.listdir(self._args.models)
+            list_of_models = [Path(self._args.models, filename) for filename in list_of_models]
+        return list_of_models
